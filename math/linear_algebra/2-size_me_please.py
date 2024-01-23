28a65a086fab9b250071bc549d4b17e8a89c1f41
Hansi Laro <hansilaro48@gmail.com>
2024-01-23 16:28:34 +0000

    add: Task 2

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3
+""" a function that calculates the shape of a matrix:"""
 
-matrix_shape = __import__('2-size_me_please').matrix_shape
 
-mat1 = [[1, 2], [3, 4]]
-print(matrix_shape(mat1))
-mat2 = [[[1, 2, 3, 4, 5], [6, 7, 8, 9, 10], [11, 12, 13, 14, 15]],
-        [[16, 17, 18, 19, 20], [21, 22, 23, 24, 25], [26, 27, 28, 29, 30]]]
-print(matrix_shape(mat2))
+def matrix_shape(matrix):
+    """ returns the shape a multi-dimensional matrix """
+    matrix_shape = []
+    while type(matrix) is list:
+        matrix_shape.append(len(matrix))
+        matrix = matrix[0]
+    
+    return matrix_shape
