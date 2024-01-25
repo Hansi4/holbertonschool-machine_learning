2674bfbbff73708e69a7e91d03dbe6ceebbcae57
Hansi Laro <hansilaro48@gmail.com>
2024-01-25 18:20:10 +0000

    Task 6

---
+++ math/linear_algebra/6-howdy_partner.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+""" defines function that calculates the shape of matrix """
+
+
+def cat_arrays(arr1, arr2):
+    """ concatenates two arrays """
+    arr1 = [1, 2, 3, 4, 5]
+    arr2 = [6, 7, 8]
+    result = arr1 + arr2
+    return result
+    
