f9b625fa04eb0865704899c51515f57e1399c54a
Hansi Laro <hansilaro48@gmail.com>
2024-01-25 15:06:13 +0000

    Task 5

---
+++ math/linear_algebra/5-across_the_planes.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+""" defines function that calculates the shape of matrix """
+
+def add_matrices2D(mat1, mat2):
+    """ adds two matrices element-wise """
+    if len(mat1) != len(mat2) or len(mat1[0] != len(mat2[0])):
+        return None
+    result=[]
+    for i in range(len(mat1)):
+        row = []
+        for j in range(len(mat1[0])):
+            row.append(mat1[i][j] + mat2[i][j])
+        result.append(row)
+    return result
