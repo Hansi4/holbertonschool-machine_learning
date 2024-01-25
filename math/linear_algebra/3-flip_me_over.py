fd5ae65355c154906979d02563dd349c2358e0e9
Hansi Laro <hansilaro48@gmail.com>
2024-01-25 17:54:10 +0000

    Task 3

--- math/linear_algebra/3-flip_me_over.py
+++ math/linear_algebra/3-flip_me_over.py
@@ -6,7 +6,6 @@ def matrix_transpose(matrix):
     """ returns the transpose of a 2D matrix, matrix """
     rows = len(matrix)
     cols = len(matrix[0])
-    
     transpose = [[0] * rows for _ in range(cols)]
     for i in range(rows):
         for j in range(cols):
