7db7c85d405737c361320699b70f8693d1fb7920
Hansi Laro <hansilaro48@gmail.com>
2024-01-25 17:57:58 +0000

    Task 4

--- math/linear_algebra/4-line_up.py
+++ math/linear_algebra/4-line_up.py
@@ -4,11 +4,9 @@
 
 def add_arrays(arr1, arr2):
     """ adds two arrays element-wise """
-
     if len(arr1) != len(arr2):
         return None
     result = []
     for i in range(len(arr1)):
         result.append(arr1[i] + arr2[i])
     return result
-    
