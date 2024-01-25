be0625ebe9060cf470e287cf8e423779abf2db74
Hansi Laro <hansilaro48@gmail.com>
2024-01-25 14:54:25 +0000

    Task 4

---
+++ math/linear_algebra/4-line_up.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+""" defines function that calculates the shape of matrix """
+
+
+def add_arrays(arr1, arr2):
+    """ adds two arrays element-wise """
+
+    if len(arr1) != len(arr2):
+        return None
+    result = []
+    for i in range(len(arr1)):
+        result.append(arr1[i] + arr2[i])
+    return result
