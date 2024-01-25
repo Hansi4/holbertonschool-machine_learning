aa21e83a7670c011f551992a74c79e06e2c50541
Hansi Laro <hansilaro48@gmail.com>
2024-01-25 18:04:53 +0000

    Task 5

--- math/linear_algebra/5-across_the_planes.py
+++ math/linear_algebra/5-across_the_planes.py
@@ -6,11 +6,10 @@ def add_matrices2D(mat1, mat2):
     """ adds two matrices element-wise """
     if (len(mat1) != len(mat2) or len(mat1[0]) != len(mat2[0])):
         return None
-    result=[]
+    result = []
     for i in range(len(mat1)):
         row = []
         for j in range(len(mat1[0])):
             row.append(mat1[i][j] + mat2[i][j])
         result.append(row)
     return result
-    
