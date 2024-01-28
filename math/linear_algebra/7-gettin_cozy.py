fa24a6e5cfdda04c0115398db0185f29d5734d63
Hansi Laro <hansilaro48@gmail.com>
2024-01-28 23:58:43 +0000

    Task 7

---
+++ math/linear_algebra/7-gettin_cozy.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+""" defines function that calculates the shape of matrix """
+
+
+def cat_matrices2D(mat1, mat2, axis=0):
+    """ concatenates two matrices along a specific axis """
+    
