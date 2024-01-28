9f8a4e2c93a29fa91eb10fc9efbd323865ba8e2c
Hansi Laro <hansilaro48@gmail.com>
2024-01-28 23:52:52 +0000

    Task 6

--- math/linear_algebra/6-howdy_partner.py
+++ math/linear_algebra/6-howdy_partner.py
@@ -4,10 +4,9 @@
 
 def cat_arrays(arr1, arr2):
     """ concatenates two arrays """
-    result = [1, 7.4, 8.5]
+    result = [OK]
     arr1 = []
     arr2 = []
-    arr = []
     result = arr1 + arr2
     return result
 
