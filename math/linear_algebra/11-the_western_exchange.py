8ccfdd88b5870a84e6baf724874165015000840a
Hansi Laro <hansilaro48@gmail.com>
2024-01-29 23:29:30 +0000

    Task 11

--- math/linear_algebra/11-the_western_exchange.py
+++ math/linear_algebra/11-the_western_exchange.py
@@ -4,8 +4,6 @@
 
 def np_transpose(matrix):
     """ transposes matrix """
-import numpy as np
-np_transpose = __import__('11-the_western_exchange').np_transpose
 matrix = np.array([[1, 2], [3, 4]])
 numpy.ndarray = np_transpose(matrix)
     return numpy.ndarray
