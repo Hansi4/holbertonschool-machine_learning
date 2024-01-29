309138b62f4ae264f9ce00ea654577e28cea1c58
Hansi Laro <hansilaro48@gmail.com>
2024-01-29 23:20:19 +0000

    Task 11

--- math/linear_algebra/11-the_western_exchange.py
+++ math/linear_algebra/11-the_western_exchange.py
@@ -4,8 +4,6 @@
 import numpy as np
 def np_transpose(matrix):
     """ transposes matrix """
-mat1 = np.array([1, 2, 3, 4, 5, 6])
-mat2 = np.array([])
 mat3 = np.array([[[1, 2, 3, 4, 5], [6, 7, 8, 9, 10]],
                  [[11, 12, 13, 14, 15], [16, 17, 18, 19, 20]]])
 transposed_matrix = np.transpose(matrix)
