c942fd99ce78ff8af1f6456fa61cf6b7b3ecc0a9
Hansi Laro <hansilaro48@gmail.com>
2024-01-29 23:22:12 +0000

    Task 11

--- math/linear_algebra/11-the_western_exchange.py
+++ math/linear_algebra/11-the_western_exchange.py
@@ -4,7 +4,5 @@
 import numpy as np
 def np_transpose(matrix):
     """ transposes matrix """
-matrix = np.array([[1, 2, 3],
-                   [4, 5, 6]])
 transposed_matrix = np.transpose(matrix)
     return transposed_matrix
