d64fa9eba05962178c0a831b032ece36ecd64043
Hansi Laro <hansilaro48@gmail.com>
2024-01-29 23:23:04 +0000

    Task 11

--- math/linear_algebra/11-the_western_exchange.py
+++ math/linear_algebra/11-the_western_exchange.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """ defines function that calculates the shape of matrix """
 
-import numpy as np
+
 def np_transpose(matrix):
     """ transposes matrix """
 transposed_matrix = np.transpose(matrix)
