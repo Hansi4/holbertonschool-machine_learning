a7d7677066a50117044a9d8ffc593f1d7c1276b1
Hansi Laro <hansilaro48@gmail.com>
2024-01-29 23:35:07 +0000

    Task 14

---
+++ math/linear_algebra/14-saddle_up.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+""" defines function that calculates the shape of matrix """
+
+
+import numpy as np
+
+def np_matmul(mat1, mat2):
+    """ performs matrix multiplication """
+
+result = np.matmul(mat1, mat2)
+    return result
