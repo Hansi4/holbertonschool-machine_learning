f6b334658b99c19e1ac4ce0a6c69bafc90dab83a
Hansi Laro <hansilaro48@gmail.com>
2024-01-30 15:43:37 +0000

    Task 8

--- math/linear_algebra/8-ridin_bareback.py
+++ math/linear_algebra/8-ridin_bareback.py
@@ -7,7 +7,7 @@ def mat_mul(mat1, mat2):
     mat1_rows, mat1_columns = len(mat1), len(mat1[0])
     mat2_rows, mat2_columns = len(mat2), len(mat2[0])
 
-    if mat1_columns != mat2_rows
+    if mat1_columns != mat2_rows:
         return None
     
     result = []
