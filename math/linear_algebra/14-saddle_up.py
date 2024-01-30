0629a0ce26c87267e7a1c3b8267b00f272a4d9e3
Hansi Laro <hansilaro48@gmail.com>
2024-01-30 15:09:21 +0000

    Task 14

--- math/linear_algebra/14-saddle_up.py
+++ math/linear_algebra/14-saddle_up.py
@@ -1,11 +1,9 @@
 #!/usr/bin/env python3
 """ performs matrix multiplication """
 
-
 import numpy as np
 
 
 def np_matmul(mat1, mat2):
     """ that performs matrix multiplication """
     return np.matmul(mat1, mat2)
-    
