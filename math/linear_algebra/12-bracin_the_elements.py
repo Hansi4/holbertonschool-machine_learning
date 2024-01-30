16ac7ef57a1c829bfa12256a640bbf61eff5592e
Hansi Laro <hansilaro48@gmail.com>
2024-01-30 14:46:30 +0000

    Task 12

--- math/linear_algebra/12-bracin_the_elements.py
+++ math/linear_algebra/12-bracin_the_elements.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
-""" a script that performs element-wise addition, subtraction, multiplication, and division"""
+""" a script that performs element-wise addition, 
+subtraction, multiplication, and division"""
 
 
 def np_elementwise(mat1, mat2):
@@ -9,4 +10,3 @@ def np_elementwise(mat1, mat2):
     mul = mat1 * mat2
     div = mat1 / mat2
     return (add, sub, mul, div)
-    
