6c2226d679dafbfef8c4ee911ec456310824ac8e
Hansi Laro <hansilaro48@gmail.com>
2024-01-30 18:35:51 +0000

    Task 13

--- math/linear_algebra/13-cats_got_your_tongue.py
+++ math/linear_algebra/13-cats_got_your_tongue.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
-""" a script that concatenates two matrices 
+""" a script that concatenates two matrices
 along a specific axis"""
-
 import numpy as np
 
 
@@ -9,4 +8,4 @@ def np_cat(mat1, mat2, axis=0):
     """ concatenates two matrices 
 along a specific axis """
     return np.concatenate((mat1, mat2), axis=axis)
-    
+  
