80027be2081a31db78ea8b47e121e7c4ac17c5d9
Hansi Laro <hansilaro48@gmail.com>
2024-02-06 13:18:24 +0000

    Task 10*

--- math/calculus/10-matisse.py
+++ math/calculus/10-matisse.py
@@ -1,7 +1,19 @@
 #!/usr/bin/env python3
-""" that calculates the derivative of a polynomial """
+""" A script that calculates the derivative of a polynomial """
 
 
 def poly_derivative(poly):
-    """ calculates the derivative of a polynomial """
+    """ A function that calculates the derivative of a polynomial """
+    if type(poly) is not list or len(poly) < 1:
+        return None
+    for coeff in poly:
+        if type(coeff) is not int:
+            return None
     
+    for power, coefficient in enumerate(poly):
+        if power == 0:
+            derivative = [0]
+        if power == 1:
+            derivative = []
+        derivative.append(power * coefficient)
+    return derivative
