f102efc0af4541cbebe58ee74d24cada145836a0
Hansi Laro <hansilaro48@gmail.com>
2024-02-06 12:54:42 +0000

    Task 9*

--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
-""" that calculates sum for i=1:n of i**2 """
+""" A script that calculates sum for i=1:n of i**2 """
 
 
 def summation_i_squared(n):
-    """ calculates sum for i=1:n of i**2 """
+    """ A function that calculates sum for i=1:n of i**2 """
     sum = (n*(n+1)*(2*n+1)) / 6
-    return sum 
+    return sum
