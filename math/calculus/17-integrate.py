0b0721cde77cfa527d64cae7d6491dec9b72a62c
Hansi Laro <hansilaro48@gmail.com>
2024-02-07 13:29:58 +0000

    Task 17*

--- math/calculus/17-integrate.py
+++ math/calculus/17-integrate.py
@@ -4,12 +4,12 @@
 
 def poly_integral(poly, C=0):
     """ A function that calculates the integral of a polynomial """
-    if type(poly) is not list or len(poly) < 1:
+    if type(poly) is not list or len(poly) == 0:
         return None
     for C in poly:
         if type(C) is not int:
             return None
-        integral = [C]
+    integral = [C]
     for power, coefficient in enumerate(poly):
         if (coefficient % (power + 1)) == 0:
             new_coefficient = coefficient // (power + 1)
