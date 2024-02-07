993bd3d401eced60670ebb7ec6b101c2cb106bbb
Hansi Laro <hansilaro48@gmail.com>
2024-02-07 13:12:20 +0000

    Task 17*

--- math/calculus/17-integrate.py
+++ math/calculus/17-integrate.py
@@ -1,7 +1,21 @@
 #!/usr/bin/env python3
-""" that calculates the integral of a polynomial """
+""" A script that calculates the integral of a polynomial """
 
 
 def poly_integral(poly, C=0):
-    """ calculates the integral of a polynomial """
-    
+    """ A function that calculates the integral of a polynomial """
+    if type(poly) is not list or len(poly) < 1:
+        return None
+    for C in poly:
+        if type(C) is not int:
+            return None
+
+    for power, coefficient in enumerate(poly):
+        if (coefficient % (power + 1)) == 0:
+            new_coefficient = coefficient // (power + 1)
+        else:
+            new_coefficient = coefficient / (power + 1)
+        integral.append(new_coefficient)
+    while integral[-1] == 0 and len(integral) > 1:
+        integral = integral[:-1]
+    return integral
