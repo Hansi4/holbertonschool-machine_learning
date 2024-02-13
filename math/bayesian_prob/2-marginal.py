a0efacc580983ce277dcfd347d23464dc6109eab
Hansi Laro <hansilaro48@gmail.com>
2024-02-13 19:46:02 +0000

    Task 2

---
+++ math/bayesian_prob/2-marginal.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+""" that calculates the marginal probability of obtaining the data """
+
+
+def marginal(x, n, P, Pr):
+    """ the marginal probability of obtaining x and n """
