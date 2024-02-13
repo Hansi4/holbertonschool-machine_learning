ed124f502232bebf26db26632afaa3a1dfb1a556
Hansi Laro <hansilaro48@gmail.com>
2024-02-13 19:43:58 +0000

    Task 1

---
+++ math/bayesian_prob/1-intersection.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""" that calculates the intersection of obtaining 
+this data with the various hypothetical probabilities """
+
+
+def intersection(x, n, P, Pr):
+    """ a 1D numpy.ndarray containing the intersection 
+    of obtaining x and n with each probability in P, respectively """
