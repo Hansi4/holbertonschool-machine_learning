cdc761cc2fbe6b62cc5ba68047fe9aa60d7fde2a
Hansi Laro <hansilaro48@gmail.com>
2024-02-13 19:47:50 +0000

    Task 3

---
+++ math/bayesian_prob/3-posterior.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""" that calculates the posterior probability for the various 
+hypothetical probabilities of developing severe side effects given the data """
+
+
+def posterior(x, n, P, Pr):
+    """ the posterior probability of each 
+    probability in P given x and n, respectively """
