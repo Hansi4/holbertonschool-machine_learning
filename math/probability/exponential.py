903d0c460a7c1bbf79489ec48558f7bdcbc37367
Hansi Laro <hansilaro48@gmail.com>
2024-02-13 14:29:30 +0000

    Task 3

--- math/probability/exponential.py
+++ math/probability/exponential.py
@@ -4,13 +4,31 @@
 
 class Exponential:
     """ Class Exponential that represents an exponential distribution """
+    
     def __init__(self, data=None, lambtha=1.):
-        self.lambtha = float(lambtha)
-        if self.lambtha <= 0:
-            raise ValueError("lambtha must be a positive value")
-        if data is not None:
+        """ represents an exponential distribution """
+        if data is None:
+            if lambtha < 0:
+                raise ValueError("lambtha must be a positive value")
+            self.lambtha = float(lambtha)
+        else:
             if type(data) is not list:
-                raise TypeError("data must be a list")
+                raise TypeError("data must be list")
             if len(data) < 2:
                 raise ValueError("data must contain multiple values")
-            self.lambtha = float(sum(data)) / len(data)
+            lambtha = float(len(data) / sum(data))
+
+
+    def pdf(self, x):
+        """ Calculates the value of the PDF for a given time period """
+        if type(k) is not int:
+            k = int(k)
+        if (k < 0):
+            return 0
+        e = 2.7182818285
+        lambtha = self.lambtha
+        factorial = 1
+        for i in range(k):
+            factorial *= (i + 1)
+        pmf = ((e ** -lambtha) * (lambtha ** k)) / factorial
+        return pmf
