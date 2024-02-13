2e6f1e504b239a1c7b9b4d87c68c1d689b62b544
Hansi Laro <hansilaro48@gmail.com>
2024-02-13 10:37:03 +0000

    Task 0

--- math/probability/poisson.py
+++ math/probability/poisson.py
@@ -13,20 +13,20 @@ def __init__(self, data=None, lambtha=1.):
             raise TypeError("data must be a list")
         if len(data) < 2:
             raise ValueError("data must contain multiple values")
-        self.lambtha = florat(sum(data)) / len(data)
+        self.lambtha = float(sum(data)) / len(data)
 
 def pmf(self, k):
     """ Calculates the value of the PMF for a given number of successes """
     if type(k) is not int:
         k = int(k)
-    if k < 0:
+    if (k < 0):
         return 0
     e = 2.7182818285
     lambtha = self.lambtha
     factorial = 1
     for i in range(k):
-        factorial *= (k + 1)
-    pmf = ( (e** -lambtha) * (lambtha ** k) ) / factorial
+        factorial *= (i + 1)
+    pmf = ((e** -lambtha) * (lambtha ** k)) / factorial
     return pmf
 
 def cdf(self, k):
