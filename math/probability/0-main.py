8e768f4da91770e7e9c1de877ebc1ba86dd7bfe1
Hansi Laro <hansilaro48@gmail.com>
2024-02-13 11:09:07 +0000

    Task 0

--- math/probability/0-main.py
+++ math/probability/0-main.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
+
 import numpy as np
 Poisson = __import__('poisson').Poisson
 
 np.random.seed(0)
-lam = np.random.uniform(0.1, 10.0)
-n = np.random.randint(100, 1000)
-data = np.random.poisson(lam, n).tolist()
-print(np.around(Poisson().lambtha, 10))
-print(np.around(Poisson(lambtha=lam).lambtha, 10))
+data = np.random.poisson(5., 100).tolist()
+p1 = Poisson(data)
+print('Lambtha:', p1.lambtha)
+
+p2 = Poisson(lambtha=5)
+print('Lambtha:', p2.lambtha)
