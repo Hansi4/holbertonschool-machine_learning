6fbfbbdf966d26206bc33d05b5348bfe011e99eb
Hansi Laro <hansilaro48@gmail.com>
2024-02-13 18:18:43 +0000

    Task 6

--- math/probability/normal.py
+++ math/probability/normal.py
@@ -10,6 +10,7 @@ class Exponential:
         if data is None:
             if stddev < 1:
                 raise ValueError("stddev must be a positive value")
+            else:
             self.stddev = float(stddev)
             self.mean = float(mean)
         else:
@@ -18,32 +19,29 @@ class Exponential:
             elif len(data) < 2:
                 raise ValueError("data must contain multiple values")
             else:
-                self.mean = sum(data) / len(data)
-
+                self.mean = float(sum(data) / len(data))
                 summation = 0
                 for x in data:
-                    summation += (x - mean) ** 2
-                stddev = (summation / len(data)) ** (1/2)
+                    summation += ((x - mean) ** 2)
+                stddev = (summation / len(data)) ** (1 / 2)
                 self.stddev = stddev
 
     def z_score(self, x):
-        """ """
+        """ Calculates the z-score of a given x-value """
         return float((x - self.mean) / self.stddev)
 
     def x_value(self, z):
-        """ """
-        return z * self.stddev + self.mean
+        """ Calculates the x-value of a given z-score """
+        return (z * self.stddev) + self.mean
 
     def pdf(self, x):
-        """
-        calculates the value of the pdf of a given x-value
-        :param x: the x-value
-        :return: pdf value for x
-        """
-        π = 3.1415926536
+        """ calculates the value of the pdf of a given x-value """
+        pi = 3.1415926536
         e = 2.7182818285
-        return ((1 / (self.stddev * ((2 * π) ** 0.5))) *
-                (e ** (-0.5 * ((x - self.mean) / self.stddev) ** 2)))
+        power = -0.5 * (self.z_score(x) ** 2)
+        coefficient = 1 / (self.stddev * ((2 * pi) ** (1 / 2)))
+        pdf = coefficient * (e ** power)
+        return pdf
 
     def cdf(self, x):
         """ Calculates the value of the CDF for a given x-value """
