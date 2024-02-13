a0b2c2fdfa1a40eadabd5db56b0d1e1b14ffd3ba
Hansi Laro <hansilaro48@gmail.com>
2024-02-13 11:32:08 +0000

    Task 1*

--- math/probability/1-main.py
+++ math/probability/1-main.py
@@ -5,8 +5,8 @@ Poisson = __import__('poisson').Poisson
 
 np.random.seed(0)
 data = np.random.poisson(5., 100).tolist()
-p1 = Poisson()
-print('P(9):', p1)
+p1 = Poisson(data)
+print('P(9):', p1.pmf(9))
 
-p2 = Poisson()
-print('P(9):', p2)
+p2 = Poisson(lambtha=5)
+print('P(9):', p2.pmf(9))
