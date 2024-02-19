a3779a487591cde1d5a761b236402b217b97309c
Hansi Laro <hansilaro48@gmail.com>
2024-02-19 19:52:20 +0000

    Task 1

--- math/bayesian_prob/1-intersection.py
+++ math/bayesian_prob/1-intersection.py
@@ -1,8 +1,18 @@
 #!/usr/bin/env python3
-""" that calculates the intersection of obtaining 
+""" that calculates the intersection of obtaining
 this data with the various hypothetical probabilities """
 
 
 def intersection(x, n, P, Pr):
-    """ a 1D numpy.ndarray containing the intersection 
+    """ a 1D numpy.ndarray containing the intersection
     of obtaining x and n with each probability in P, respectively """
+    if not isinstance(n, int) or n <= 0:
+        raise ValueError("n must be a positive integer")
+    if not isinstance(x, int) or int(x) < 0:
+        raise ValueError("x must be an integer that is greater than or equal to 0")
+    if int(x) > n:
+        raise ValueError("x cannot be greater than n")
+    if not isinstance(P, np.ndarray) or P.ndim != 1:
+        raise TypeError("P must be a 1D numpy.ndarray")
+    if not isinstance(Pr, np.ndarray) or Pr.shape != P.shape:
+        raise TypeError("Pr must be a numpy.ndarray with the same shape as P")
