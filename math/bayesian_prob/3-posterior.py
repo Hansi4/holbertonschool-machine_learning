c1a0d90a82ccf740db933a441c00b24fc74b5bad
Hansi Laro <hansilaro48@gmail.com>
2024-02-19 20:09:02 +0000

    Task 3

--- math/bayesian_prob/3-posterior.py
+++ math/bayesian_prob/3-posterior.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
-""" that calculates the posterior probability for the various 
+""" that calculates the posterior probability for the various
 hypothetical probabilities of developing severe side effects given the data """
 
 import numpy as np
 
 
 def posterior(x, n, P, Pr):
-    """ the posterior probability of each 
+    """ the posterior probability of each
     probability in P given x and n, respectively """
     if not isinstance(n, int) or n <= 0:
         raise ValueError("n must be a positive integer")
