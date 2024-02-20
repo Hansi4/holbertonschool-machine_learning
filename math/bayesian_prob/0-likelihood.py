9a4b076ffce005cc6951b379199e51d78615fcef
Hansi Laro <hansilaro48@gmail.com>
2024-02-20 15:52:24 +0000

    Task 0

--- math/bayesian_prob/0-likelihood.py
+++ math/bayesian_prob/0-likelihood.py
@@ -13,10 +13,16 @@ def likelihood(x, n, P):
     if not isinstance(n, int) or n <= 0:
         raise ValueError("n must be a positive integer")
     if not isinstance(x, int) or int(x) < 0:
-        raise ValueError("x must be an integer that is greater than or equal to 0")
+        raise ValueError(
+            "x must be an integer that is greater than or equal to 0")
     if int(x) > n:
         raise ValueError("x cannot be greater than n")
     if not isinstance(P, np.ndarray) or P.ndim != 1:
         raise TypeError("P must be a 1D numpy.ndarray")
     if not np.all((P >= 0) & (P <= 1)):
         raise ValueError("All values in P must be in the range [0, 1]")
+
+    factorial = np.math.factorial
+    fact_coefficient = factorial(n) / (factorial(x) * factorial(n-x))
+    likelihood = fact_coefficient * (P ** x) * ((1 - P) ** (n - x))
+    return likelihood
