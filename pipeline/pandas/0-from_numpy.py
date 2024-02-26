5117a07fcd184997c7f40879398ceeb91be0a001
Hansi Laro <hansilaro48@gmail.com>
2024-02-26 18:04:45 +0000

    Task 0

---
+++ pipeline/pandas/0-from_numpy.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""" A script that creates a pd.DataFrame from a np.ndarray """
+
+
+def from_numpy(array):
+    """ A function that creates a pd.DataFrame from a np.ndarray """
+    
+    return pd.DataFrame
