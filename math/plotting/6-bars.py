e48ab20e042bda32a6b309c5619b79f572ee3561
Hansi Laro <hansilaro48@gmail.com>
2024-02-26 17:22:34 +0000

    Task 6

--- math/plotting/6-bars.py
+++ math/plotting/6-bars.py
@@ -5,25 +5,26 @@ import matplotlib.pyplot as plt
 np.random.seed(5)
 fruit = np.random.randint(0, 20, (4,3))
 
-fruit = np.array([[10, 15, 7],
-                  [5, 8, 12],
-                  [12, 10, 5],
-                  [7, 10, 15]])
-
 colors = ['red', 'yellow', '#ff8000', '#ffe5b4']
 fruits = ['apples', 'bananas', 'oranges', 'peaches']
 persons = ['Farrah', 'Fred', 'Felicia']
 
-# Stacked bar plot
 fig, ax = plt.subplots()
+width = 0.5
+bottom = np.zeros(len(persons))
 
-for i in range(len(fruit)):
-    bottom = np.sum(fruit[:i], axis=0) if i > 0 else 0
-    ax.bar(persons, fruit[i], bottom=bottom, label=fruits[i], color=colors[i], width=0.5)
+for i in range(len(fruits)):
+    ax.bar(
+        x = persons,
+        height = fruit[i],
+        width = width,
+        bottom = bottom,
+        label = fruits[i],
+        color = colors[i])
+    bottom += fruit[i]
 
 ax.set_ylabel('Quantity of Fruit')
 ax.set_title("Number of Fruit per Person")
-ax.legend()
-ax.set_ylim(0, 80)
 ax.set_yticks(np.arange(0, 81, 10))
+plt.legend()
 plt.show()
