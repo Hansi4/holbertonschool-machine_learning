a87439c08896233d1b98d505f40e25d75c89d80f
Hansi Laro <hansilaro48@gmail.com>
2024-02-26 18:11:49 +0000

    Task 2

---
+++ pipeline/pandas/2-from_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""" A script that loads data from a file as a pd.DataFrame """
+
+
+def from_file(filename, delimiter):
+    """ A function that loads data from a file as a pd.DataFrame """
+    
+    return pd.DataFrame
