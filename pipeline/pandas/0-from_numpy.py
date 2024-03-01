395a870400c9c306eee0eacbded9c1e90121fd34
Hansi Laro <hansilaro48@gmail.com>
2024-03-01 20:22:03 +0000

    Task 0

--- pipeline/pandas/0-from_numpy.py
+++ pipeline/pandas/0-from_numpy.py
@@ -1,8 +1,14 @@
 #!/usr/bin/env python3
-""" A script that creates a pd.DataFrame from a np.ndarray """
+""" Defines function that creates a Pandas DataFrame from a Numpy ndarray """
+
+
+import pandas as pd
+import string
 
 
 def from_numpy(array):
     """ A function that creates a pd.DataFrame from a np.ndarray """
-    
-    return pd.DataFrame
+    alphabet = list(string.ascii_uppercase)
+    column_labels = [alphabet[i] for i in range(len(array[0]))]
+    df = pd.DataFrame(array, columns=column_labels)
+    return df
