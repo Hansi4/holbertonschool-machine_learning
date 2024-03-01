b77ba707ec736616c92d942a777dc9d71427527c
Hansi Laro <hansilaro48@gmail.com>
2024-03-01 20:30:54 +0000

    Task 1

--- pipeline/pandas/1-from_dictionary.py
+++ pipeline/pandas/1-from_dictionary.py
@@ -2,7 +2,17 @@
 """ A script that created a pd.DataFrame from a dictionary """
 
 
+import pandas as pd
 
+
+def from_dictionary():
     """ A python script that created a pd.DataFrame from a dictionary """
-    
-    
+    data = {
+        'First': [0.0, 0.5, 1.0, 1.5],
+        'Second': ['one', 'two', 'three', 'four']
+    }
+
+    index_labels = list("ABCD")
+
+    df = pd.DataFrame(data, index=index_labels)
+    return df
