0d590724af4b7dcbf20372958d02b891726ce12e
Hansi Laro <hansilaro48@gmail.com>
2024-03-03 14:10:10 +0000

    Task 2

--- pipeline/pandas/2-from_file.py
+++ pipeline/pandas/2-from_file.py
@@ -7,6 +7,6 @@ import pandas as pd
 
 def from_file(filename, delimiter):
     """ A function that loads data from a file as a pd.DataFrame """
-    data = pd.read_csv(filename, delimiter)
+    data = pd.read_csv(filename, delimiter='\t')
     df = pd.DataFrame(data)
     return df
