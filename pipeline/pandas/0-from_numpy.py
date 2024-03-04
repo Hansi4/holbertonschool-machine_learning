6777f2aba2910c7b6c506b89f066228aedd62974
Hansi Laro <hansilaro48@gmail.com>
2024-03-04 18:15:15 +0000

    Task 0

--- pipeline/pandas/0-from_numpy.py
+++ pipeline/pandas/0-from_numpy.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
-""" Defines function that creates a Pandas DataFrame from a Numpy ndarray """
+""" Defines function that creates 
+a Pandas DataFrame from a Numpy ndarray """
 
 
 import pandas as pd
@@ -10,5 +11,5 @@ def from_numpy(array):
     """ A function that creates a pd.DataFrame from a np.ndarray """
     alphabet = list(string.ascii_uppercase)
     column_labels = [alphabet[i] for i in range(len(array[0]))]
-    df = pd.DataFrame(array, columns=column_labels)
+    df = pd.DataFrame(data=array, columns=column_labels)
     return df
