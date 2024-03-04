5995af56f7f4df7ae08e44a77e21b8ee7ca0927c
Hansi Laro <hansilaro48@gmail.com>
2024-03-04 19:00:25 +0000

    Task 4

--- pipeline/pandas/4-array.py
+++ pipeline/pandas/4-array.py
@@ -5,7 +5,6 @@ from_file = __import__('2-from_file').from_file
 
 df = from_file('coinbaseUSD_1-min_data_2014-12-01_to_2019-01-09.csv', ',')
 
-last_10_rows = df[['High', 'Close']].tail(10)
-A = last_10_rows.to_numpy()
+A = df[['High', 'Close']].tail(10).to_numpy()
 
 print(A)
