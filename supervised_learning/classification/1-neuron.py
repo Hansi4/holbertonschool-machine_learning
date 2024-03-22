4b69dc1ebcfd0231b331e536f1369d461c01b3c3
Hansi Laro <hansilaro48@gmail.com>
2024-03-22 00:02:47 +0000

    Task 1

--- supervised_learning/classification/1-neuron.py
+++ supervised_learning/classification/1-neuron.py
@@ -1,1 +1,26 @@
-hi
+#!/usr/bin/env python3
+""" that defines a single neuron performing binary classification """
+
+import numpy as np
+
+class Neuron:
+    def __init__(self, nx):
+        """ that defines a single neuron performing binary classification """
+        if not isinstance(nx, int):
+            raise TypeError("nx must be a integer")
+        if nx < 1:
+            raise ValueError("nx must be positive")
+    
+        self.W = np.random.randn(nx)
+        self.b = 0
+        self.A = 0
+
+    
+    def get_W(self):
+        return self.W
+
+    def get_b(self):
+        return self.b
+
+    def get_A(self):
+        return self.A
