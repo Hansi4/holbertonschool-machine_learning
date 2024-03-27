84b62c540bae3cd3acf9c01615f9c10e527b3c3b
Hansi Laro <hansilaro48@gmail.com>
2024-03-27 23:09:57 +0000

    Task 10

--- supervised_learning/classification/10-neural_network.py
+++ supervised_learning/classification/10-neural_network.py
@@ -61,7 +61,8 @@ class NeuralNetwork:
     def forward_prop(self, X):
         """ Calculates the forward propagation of the neural network """
         z1 = np.matmul(self.W1, X) + self.b1
-        self.__A1 = 1 / (1 + np.exp(-z1))
+        self.__A1 = 1 / (1 + (np.exp(-z1)))
         z2 = np.matmul(self.W2, X) + self.b2
-        self.__A2 = 1 / (1 + np.exp(-z2))
-        return (self.__A1, self.__A2)
+        self.__A2 = 1 / (1 + (np.exp(-z2)))
+        return self.__A1
+        return self.__A2
