6d7eb01804f8618cfdad9e200e525464e9a6d0ce
Hansi Laro <hansilaro48@gmail.com>
2024-03-28 15:40:17 +0000

    Task 10

--- supervised_learning/classification/10-neural_network.py
+++ supervised_learning/classification/10-neural_network.py
@@ -21,21 +21,23 @@ class NeuralNetwork:
             raise TypeError("nodes must be an integer")
         if nodes < 1:
             raise ValueError("nodes must be a positive integer")
+
         self.__W1 = np.random.randn(nodes, nx)
         self.__b1 = np.zeros((nodes, 1))
         self.__A1 = 0
+
         self.__W2 = np.random.randn(1, nodes)
         self.__b2 = 0
         self.__A2 = 0
 
     @property
     def W1(self):
-        """ Get method for property Weights """
+        """ Get method for property W1 """
         return self.__W1
 
     @property
     def b1(self):
-        """ Get method for property Bias """
+        """ Get method for property b1 """
         return self.__b1
 
     @property
@@ -45,12 +47,12 @@ class NeuralNetwork:
 
     @property
     def W2(self):
-        """ Get method for property Weights """
+        """ Get method for property W2 """
         return self.__W2
 
     @property
     def b2(self):
-        """ Get method for property Bias """
+        """ Get method for property b2 """
         return self.__b2
 
     @property
@@ -62,6 +64,8 @@ class NeuralNetwork:
         """ Calculates the forward propagation of the neural network """
         z1 = np.matmul(self.W1, X) + self.b1
         self.__A1 = 1 / (1 + (np.exp(-z1)))
+
         z2 = np.matmul(self.W2, self.A1) + self.b2
         self.__A2 = 1 / (1 + (np.exp(-z2)))
+
         return (self.__A1, self.__A2)
