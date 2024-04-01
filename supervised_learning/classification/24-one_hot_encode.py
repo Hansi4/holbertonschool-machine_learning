952bdd1d0e09f1d858da481caec781bec70e6b85
Hansi Laro <hansilaro48@gmail.com>
2024-04-01 09:38:15 +0000

    Task 24

--- supervised_learning/classification/24-one_hot_encode.py
+++ supervised_learning/classification/24-one_hot_encode.py
@@ -1,1 +1,18 @@
-hi
+#!/usr/bin/env python3
+""" One hot encoding """
+
+
+import numpy as np
+
+
+def one_hot_encode(Y, classes):
+    """ one hot encode """
+    if not isinstance(Y, np.ndarray) or len(Y.shape) != 1:
+        return None
+    if not isinstance(classes, int) or classes < np.max(Y) + 1:
+        return None
+
+    encoded_array = np.zeros((classes, Y.size), dtype=float)
+    encoded_array[Y, np.arange(Y.size)] = 1
+    return encoded_array
+
