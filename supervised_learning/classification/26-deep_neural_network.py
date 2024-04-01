3ae1a8fdfc87aaf2265915d7bb7c07f282639679
Hansi Laro <hansilaro48@gmail.com>
2024-04-01 16:46:05 +0000

    Task 26

--- supervised_learning/classification/26-deep_neural_network.py
+++ supervised_learning/classification/26-deep_neural_network.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 """
-Class Deep Neural Network that defines a
-deep neural network performing binary classification
+DeepNeuralNetwork performing binary classification
 """
 
 
@@ -9,10 +8,15 @@ import numpy as np
 
 
 class DeepNeuralNetwork:
-    """ Defines a deep neural network performing binary classification """
+    """
+    class that represents a deep neural network
+    """
+
     def __init__(self, nx, layers):
-        """ Class constructor """
-        if not isinstance(nx, int):
+        """
+        class constructor
+        """
+        if type(nx) is not int:
             raise TypeError("nx must be an integer")
         if nx < 1:
             raise ValueError("nx must be a positive integer")
@@ -37,55 +41,63 @@ class DeepNeuralNetwork:
 
     @property
     def L(self):
-        """ Gets the private instance """
-        return self.__L
+        """
+        gets the private instance attribute __L
+        """
+        return (self.__L)
 
     @property
     def cache(self):
-        """ Get method for property b1 """
-        return self.__cache
+        """
+        gets the private instance attribute __cache
+        """
+        return (self.__cache)
 
     @property
     def weights(self):
-        """ Get method for property b1 """
-        return self.__weights
+        """
+        gets the private instance attribute __weights
+        """
+        return (self.__weights)
 
     def forward_prop(self, X):
-        """ Calculates the forward propagation of the neural network """
+        """
+        calculates the forward propagation of the neuron
+        """
         self.__cache["A0"] = X
-
         for index in range(self.L):
             W = self.weights["W{}".format(index + 1)]
             b = self.weights["b{}".format(index + 1)]
-
             z = np.matmul(W, self.cache["A{}".format(index)]) + b
-            A = 1 / (1 + np.exp(-z))
-
-            self.cache["A{}".format(index + 1)] = A
-
-        return A, self.cache
+            A = 1 / (1 + (np.exp(-z)))
+            self.__cache["A{}".format(index + 1)] = A
+        return (A, self.cache)
 
     def cost(self, Y, A):
-        """ Calculates the cost of the model using logistic regression """
+        """
+        calculates the cost of the model using logistic regression
+        """
         m = Y.shape[1]
-        m_loss = np.sum((Y * np.log(A)) + (1 - Y) * np.log(1.0000001 - A))
-        cost = (1/m) * (-(m_loss))
-        return cost
+        m_loss = np.sum((Y * np.log(A)) + ((1 - Y) * np.log(1.0000001 - A)))
+        cost = (1 / m) * (-(m_loss))
+        return (cost)
 
     def evaluate(self, X, Y):
-        """ Evaluate function for deep neural network  """
+        """
+        evaluates the neural network's predictions
+        """
         A, cache = self.forward_prop(X)
         cost = self.cost(Y, A)
         prediction = np.where(A >= 0.5, 1, 0)
-        return [prediction, cost]
+        return (prediction, cost)
 
     def gradient_descent(self, Y, cache, alpha=0.05):
-        """ Calculates one pass of gradient descent on the neural network """
+        """
+        calculate the gradient descent
+        """
         m = Y.shape[1]
         back = {}
-
         for index in range(self.L, 0, -1):
-
             A = cache["A{}".format(index - 1)]
             if index == self.L:
                 back["dz{}".format(index)] = (cache["A{}".format(index)] - Y)
@@ -95,21 +107,20 @@ class DeepNeuralNetwork:
                 back["dz{}".format(index)] = (
                     np.matmul(W_prev.transpose(), dz_prev) *
                     (A_current * (1 - A_current)))
-
             dz = back["dz{}".format(index)]
             dW = (1 / m) * (np.matmul(dz, A.transpose()))
             db = (1 / m) * np.sum(dz, axis=1, keepdims=True)
             W_prev = self.weights["W{}".format(index)]
-
             self.__weights["W{}".format(index)] = (
                 self.weights["W{}".format(index)] - (alpha * dW))
             self.__weights["b{}".format(index)] = (
                 self.weights["b{}".format(index)] - (alpha * db))
 
     def train(self, X, Y, iterations=5000, alpha=0.05,
-    verbose=True, graph=True, step=100):
-        """ Trains the deep neural network by updating
-        the private attributes __weights and __cache """
+              verbose=True, graph=True, step=100):
+        """
+        trains the neuron and updates __weights and __cache
+        """
         if type(iterations) is not int:
             raise TypeError("iterations must be an integer")
         if iterations <= 0:
@@ -152,7 +163,9 @@ class DeepNeuralNetwork:
         return self.evaluate(X, Y)
 
     def save(self, filename):
-        """ Saves the instance object to a file in pickle format """
+        """
+        saves the instance object to a file in pickle format
+        """
         import pickle
         if not isinstance(filename, str):
             print("Error: filename must be a string.")
@@ -166,7 +179,9 @@ class DeepNeuralNetwork:
 
     @staticmethod
     def load(filename):
-        """ Loads a pickled DeepNeuralNetwork object from a file"""
+        """
+        loads a pickled DeepNeuralNetwork object from a file
+        """
         import pickle
         try:
             with open(filename, 'rb') as f:
