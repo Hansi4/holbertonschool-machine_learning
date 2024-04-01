c2e75b28b6549987131d578457793ced9a778ad9
Hansi Laro <hansilaro48@gmail.com>
2024-04-01 10:04:55 +0000

    Task 24

--- supervised_learning/classification/24-one_hot_encode.py
+++ supervised_learning/classification/24-one_hot_encode.py
@@ -1,18 +1,20 @@
 #!/usr/bin/env python3
-""" One hot encoding """
+""" One Hot Encoding """
 
 
 import numpy as np
 
 
 def one_hot_encode(Y, classes):
-    """ one hot encode """
+    """ Method that converts a numerical label vector into a
+        one-hot vector """
     if not isinstance(Y, np.ndarray) or len(Y.shape) != 1:
         return None
     if not isinstance(classes, int) or classes < np.max(Y) + 1:
         return None
 
     encoded_array = np.zeros((classes, Y.size), dtype=float)
-    encoded_array[Y, np.arange(Y.size)] = 1
-    return encoded_array
 
+    encoded_array[Y, np.arange(Y.size)] = 1
+
+    return encoded_array
