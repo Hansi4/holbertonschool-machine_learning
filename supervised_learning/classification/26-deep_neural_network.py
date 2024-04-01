5b844efe5f923349604823e4cd740ed2eadd7301
Hansi Laro <hansilaro48@gmail.com>
2024-04-01 10:07:13 +0000

    Task 26

--- supervised_learning/classification/26-deep_neural_network.py
+++ supervised_learning/classification/26-deep_neural_network.py
@@ -1,1 +1,23 @@
-hi
+
+
+
+
+def save(self, filename):
+    import pickle
+
+    if not isinstance
+
+    if not filename
+
+
+ @staticmethod
+ def load(filename):
+    import pickle
+
+    try:
+        with open(filename, 'rb') as f:
+            obj = pickle.load(f)
+            return obj
+    except FileNotFoundError:
+        print("Filename does not exist")
+        return
