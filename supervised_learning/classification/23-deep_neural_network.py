f5e297900f53327858d737c48eab001a645bd539
Hansi Laro <hansilaro48@gmail.com>
2024-04-01 16:31:50 +0000

    Task 23

--- supervised_learning/classification/23-deep_neural_network.py
+++ supervised_learning/classification/23-deep_neural_network.py
@@ -128,20 +128,20 @@ class DeepNeuralNetwork:
             x_points = np.arange(0, iterations + 1, step)
             points = []
         for itr in range(iterations):
-            A1, A2 = self.forward_prop(X)
+            A, cache = self.forward_prop(X)
             if verbose and (itr % step) == 0:
-                cost = self.cost(Y, A2)
+                cost = self.cost(Y, A)
                 print("Cost after " + str(itr) + " iterations: " + str(cost))
             if graph and (itr % step) == 0:
-                cost = self.cost(Y, A2)
+                cost = self.cost(Y, A)
                 points.append(cost)
-            self.gradient_descent(X, Y, A1, A2, alpha)
+            self.gradient_descent(Y, cache, alpha)
         itr += 1
         if verbose:
-            cost = self.cost(Y, A2)
+            cost = self.cost(Y, A)
             print("Cost after " + str(itr) + " iterations: " + str(cost))
         if graph:
-            cost = self.cost(Y, A2)
+            cost = self.cost(Y, A)
             points.append(cost)
             y_points = np.asarray(points)
             plt.plot(x_points, y_points, 'b')
