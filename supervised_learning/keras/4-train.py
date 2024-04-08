bada91510e462e699dff8f383936a57b4ebdb6f4
Hansi Laro <hansilaro48@gmail.com>
2024-04-08 14:56:36 +0000

    Task 4

--- supervised_learning/keras/4-train.py
+++ supervised_learning/keras/4-train.py
@@ -7,7 +7,7 @@ import tensorflow.keras as K
 
 
 def train_model(network, data, labels, batch_size,
-epochs, verbose=True, shuffle=False):
+                epochs, verbose=True, shuffle=False):
     """ A python function that trains a model
     using mini-batch gradient descent """
 
