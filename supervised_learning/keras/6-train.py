46c6409c3315aaf984d0f789b745de2382d36240
Hansi Laro <hansilaro48@gmail.com>
2024-04-08 18:47:10 +0000

    Task 6

---
+++ supervised_learning/keras/6-train.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+""" A function to also train the model using early stopping """
+
+
+import tensorflow.keras as K
+
+
+def train_model(network, data, labels, batch_size, epochs,
+                validation_data=None, early_stopping=False,
+                patience=0, verbose=True, shuffle=False):
+    """ A python function to also train the model using early stopping """
+    callback
+    if
+    
+    history = network.fit(x=data,
+                          y=labels,
+                          batch_size=batch_size,
+                          epochs=epochs,
+                          validation_data=validation_data,
+                          callbacks=[callback],
+                          verbose=verbose,
+                          shuffle=shuffle)
+
+    return history
