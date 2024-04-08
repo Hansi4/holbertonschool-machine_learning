b87145b51d016ca558a266b39abd9f64bcdb416c
Hansi Laro <hansilaro48@gmail.com>
2024-04-08 14:29:36 +0000

    Task 2

--- supervised_learning/keras/2-optimize.py
+++ supervised_learning/keras/2-optimize.py
@@ -9,8 +9,8 @@ import tensorflow.keras as K
 
 def optimize_model(network, alpha, beta1, beta2):
     """ A python function that sets up Adam optimization for a
-keras model with categorical crossentropy
-loss and accuracy metrics """
+    keras model with categorical crossentropy
+    loss and accuracy metrics """
 
     Adam_optimizer = K.optimizers.Adam(
         learning_rate=alpha, beta_1=beta1, beta_2=beta2)
