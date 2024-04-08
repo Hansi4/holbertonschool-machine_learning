5a47abe50cfa3afe9337474937d0c0fae38bd910
Hansi Laro <hansilaro48@gmail.com>
2024-04-08 14:53:19 +0000

    Task 5

--- supervised_learning/keras/5-train.py
+++ supervised_learning/keras/5-train.py
@@ -9,8 +9,12 @@ def train_model(network, data, labels, batch_size,
                 epochs, validation_data=None, verbose=True, shuffle=False):
     """ A python function to also analyze validaiton data"""
 
-    history = network.fit(x=data, y=labels, batch_size=batch_size,
-    epochs=epochs, validation_data=validation_data,
-    verbose=verbose, shuffle=shuffle)
+    history = network.fit(x=data,
+                          y=labels,
+                          batch_size=batch_size,
+                          epochs=epochs,
+                          validation_data=validation_data,
+                          verbose=verbose,
+                          shuffle=shuffle)
 
     return history
