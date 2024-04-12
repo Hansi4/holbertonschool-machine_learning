cb33eb59d25bcf9342df4ca0dae00cb15994bfbb
Hansi Laro <hansilaro48@gmail.com>
2024-04-12 09:17:07 +0000

    Task 6

--- supervised_learning/keras/6-train.py
+++ supervised_learning/keras/6-train.py
@@ -9,9 +9,11 @@ def train_model(network, data, labels, batch_size, epochs,
                 validation_data=None, early_stopping=False,
                 patience=0, verbose=True, shuffle=False):
     """ A python function to also train the model using early stopping """
-    callback
-    if
-    
+    callback = []
+
+    if early_stopping is True and validation_data is not None:
+        callback = K.callbacks.EarlyStopping(monitor='val_loss', patience=patience)
+
     history = network.fit(x=data,
                           y=labels,
                           batch_size=batch_size,
