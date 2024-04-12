4173bfa706011fb4b2c2f3255df8d62103f243f4
Hansi Laro <hansilaro48@gmail.com>
2024-04-12 09:24:50 +0000

    Task 7

--- supervised_learning/keras/7-train.py
+++ supervised_learning/keras/7-train.py
@@ -11,13 +11,25 @@ def train_model(network, data, labels, batch_size, epochs,
                 decay_rate=1, verbose=True, shuffle=False):
     """ A python function to also train the model
     with learning rate decay """
-    callback
-    if
-    
-    
-    
-    
-    
+    callback = []
+
+    if early_stopping is True and validation_data is not None:
+        early_stop = K.callbacks.EarlyStopping(monitor='val_loss', patience=patience)
+
+        callback.append(early_stop)
+
+    if learning_rate_decay and validation_data:
+
+        def scheduler(epochs):
+            lr = alpha / (1 + decay_rate * epochs)
+            return lr
+
+        inv_time_decay = K.callbacks.LearningRateScheduler(
+            scheduler,
+            verbose=1
+        )
+
+        callback.append(inv_time_decay)
     
     history = network.fit(x=data,
                           y=labels,
