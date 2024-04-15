1a851a47eca43cffc497d92796a3e5cae47c072f
Hansi Laro <hansilaro48@gmail.com>
2024-04-15 15:14:14 +0000

    Task 11

---
+++ supervised_learning/optimization/11-learning_rate_decay.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+""" Learning Rate Decay """
+
+
+import numpy as np
+
+
+def learning_rate_decay(alpha, decay_rate, global_step, decay_step):
+    """ A python function that updates the learning rate
+    using inverse time decay in numpy """
+
+    
+
+    return 
