bc210da52ea55df962718f95c2dbd2906eec8480
Hansi Laro <hansilaro48@gmail.com>
2024-04-15 14:03:30 +0000

    Task 3

---
+++ supervised_learning/optimization/3-mini_batch.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+""" Mini-Batch """
+
+
+import numpy as np
+
+
+def train_mini_batch(X_train, Y_train, X_valid, Y_valid,
+                     batch_size=32, epochs=5,
+                     load_path="/tmp/model.ckpt", save_path="/tmp/model.ckpt"):
+    """ A python function that trains a loaded
+    neural network model using mini-batch gradient descent """
+    
+
+
+    return model
