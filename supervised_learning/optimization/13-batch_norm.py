4431d721e6dac2d9263d7e2db7620edaac27fefe
Hansi Laro <hansilaro48@gmail.com>
2024-04-15 15:17:56 +0000

    Task 13

---
+++ supervised_learning/optimization/13-batch_norm.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+""" Batch Normalization """
+
+
+import numpy as np
+
+
+def batch_norm(Z, gamma, beta, epsilon):
+    """ A python function that normalizes an unactivated output
+    of a neural network using batch normalization """
+
+    
+
+    return 
