dda8bcad78f426f0adebd1c8cbac5d7592f9bc33
Hansi Laro <hansilaro48@gmail.com>
2024-04-15 14:52:42 +0000

    Task 8

--- supervised_learning/optimization/8-RMSProp.py
+++ supervised_learning/optimization/8-RMSProp.py
@@ -6,7 +6,8 @@ import tensorflow.compat.v1 as tf
 
 
 def create_RMSProp_op(loss, alpha, beta2, epsilon):
-    """ A python function that creates the training operation for a neural network
+    """ A python function that creates the
+    training operation for a neural network
     in tensorflow using the RMSProp optimization algorithm """
 
     optimizer = tf.train.RMSPropOptimizer(
