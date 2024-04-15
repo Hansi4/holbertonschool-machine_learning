168a7f79e8e83ee330c489f652edbffbcdec32da
Hansi Laro <hansilaro48@gmail.com>
2024-04-15 15:19:59 +0000

    Task 14

---
+++ supervised_learning/optimization/14-batch_norm.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+""" Batch Normalization Upgraded """
+
+
+import tensorflow.compat.v1 as tf
+
+
+def create_batch_norm_layer(prev, n, activation):
+    """ A python function that creates a batch normalization
+    layer for a neural network in tensorflow """
+
+    
+
+    return 
