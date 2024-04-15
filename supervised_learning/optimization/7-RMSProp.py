ac09fba76e10e5dc62a372face8052d76bf03d54
Hansi Laro <hansilaro48@gmail.com>
2024-04-15 14:44:22 +0000

    Task 7

---
+++ supervised_learning/optimization/7-RMSProp.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+""" RMSProp """
+
+
+import tensorflow.compat.v1 as tf
+
+
+def update_variables_RMSProp(alpha, beta2, epsilon, var, grad, s):
+    """ A python function that updates a
+    variable using the RMSProp optimization algorithm """
+
+    squared_gradient = beta2 * s + (1 - beta2) * (grad ** 2)
+    update_var = var - (alpha * grad) / (np.sqrt(squared_gradient) + epsilon)
+
+    return update_var, squared_gradient
