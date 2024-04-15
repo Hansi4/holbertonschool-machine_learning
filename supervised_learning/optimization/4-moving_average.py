9a54a7703a1397572f8af08846008c8c77d3e09a
Hansi Laro <hansilaro48@gmail.com>
2024-04-15 14:09:03 +0000

    Task 4

---
+++ supervised_learning/optimization/4-moving_average.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+""" Moving Average """
+
+
+import numpy as np
+
+
+def moving_average(data, beta):
+    """ A python function that calculates
+    the weighted moving average of a data set """
+    m_av = []
+    w = 0
+
+    for i, d in enumerate(data):
+        # update weight average
+        w = beta * w + (1 - beta) * d
+        # bias correction
+        w_new = w / (i - beta**(i+1))
+        # add moving average to list
+        m_av.append(w_new)
+
+    return m_av
