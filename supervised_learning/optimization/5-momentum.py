f626bb9b8564fa893802d6fccc0a46bdfba49355
Hansi Laro <hansilaro48@gmail.com>
2024-04-15 14:21:46 +0000

    Task 5

---
+++ supervised_learning/optimization/5-momentum.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+""" Momentum """
+
+
+import numpy as np
+
+
+def update_variables_momentum(alpha, beta1, var, grad, v):
+    """ A python function that updates a variable
+    using the gradient descent with
+    momentum optimization algorithm """
+
+    # formula for momentum
+    dW = beta1 * v + (1 - beta1) * grad
+    
+    # update variable
+    var_new = var - dW * alpha
+
+    return var_new, dW
