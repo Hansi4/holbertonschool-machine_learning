614d9edd0ab6677cf75ca193f107a2ebd4f3e52f
Hansi Laro <hansilaro48@gmail.com>
2024-04-15 14:35:54 +0000

    Task 6

---
+++ supervised_learning/optimization/6-momentum.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+""" Momentum upgraded """
+
+
+import tensorflow.compat.v1. as tf
+
+
+def create_momentum_op(loss, alpha, beta1):
+    """ A python function that creates the training operation
+    for a neural network in tensorflow using the gradient
+    descent with momentum optimization algorithm """
+
+    optimizer = tf.train.MomentumOptimizer(
+        learning_rate=alpha,
+        momentum=beta1
+    )
+    train_op = optimizer.minimize(loss)
+
+    return train_op
