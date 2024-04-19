e2f29aaf516da55874dd266475ed9378b0cf3d48
Hansi Laro <hansilaro48@gmail.com>
2024-04-19 18:31:39 +0000

    Task 12

--- supervised_learning/optimization/12-learning_rate_decay.py
+++ supervised_learning/optimization/12-learning_rate_decay.py
@@ -8,7 +8,13 @@ import tensorflow.compat.v1 as tf
 def learning_rate_decay(alpha, decay_rate, global_step, decay_step):
     """ A python function that creates a learning rate
     decay operation in tensorflow using inverse time decay """
+    # set train exponential decay in tf
+    # use staircase=True to occur in a stepwise fashion
+    learning_rate = tf.compat.v1.train.inverse_time_decay(
+        learning_rate=alpha,
+        decay_steps=decay_step,
+        decay_rate=decay_rate,
+        global_step=global_step,
+        staircase=True)
 
-    
-
-    return 
+    return learning_rate
