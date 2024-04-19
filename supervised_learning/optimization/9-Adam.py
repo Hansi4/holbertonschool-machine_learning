e83390f43993f777037dd8b9a5e4649ce2a320f4
Hansi Laro <hansilaro48@gmail.com>
2024-04-19 18:09:58 +0000

    Task 9

--- supervised_learning/optimization/9-Adam.py
+++ supervised_learning/optimization/9-Adam.py
@@ -9,6 +9,15 @@ def update_variables_Adam(alpha, beta1, beta2, epsilon, var, grad, v, s, t):
     """ A python function that updates a variable
     in place using the Adam optimization algorithm """
 
-    
+    # update rules
+    new_v = beta1 * v + (1 - beta1) * grad
+    new_s = beta2 * s + (1 - beta2) * grad**2
 
-    return 
+    # bias correction
+    v_corrected = new_v / (1 - beta1**t)
+    s_corrected = new_s / (1 - beta2**t)
+
+    # update var
+    var = var - alpha * (v_corrected / (np.sqrt(s_corrected) + epsilon))
+
+    return var, new_v, new_s
