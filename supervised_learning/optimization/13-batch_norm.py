00cc169ef72848e733bcc0eced00384eeddf08b7
Hansi Laro <hansilaro48@gmail.com>
2024-04-19 18:46:01 +0000

    Task 13

--- supervised_learning/optimization/13-batch_norm.py
+++ supervised_learning/optimization/13-batch_norm.py
@@ -8,7 +8,15 @@ import numpy as np
 def batch_norm(Z, gamma, beta, epsilon):
     """ A python function that normalizes an unactivated output
     of a neural network using batch normalization """
+    # compute mean and variance
+    mean = np.mean(Z, axis=0)
+    variance = np.var(Z, axis=0)
+    std_dev = np.sqrt(variance + epsilon)
 
-    
+    # normalize : - mean / root square variance
+    Z_norm = (Z - mean) / std_dev
 
-    return 
+    # scale by gama and shifted by beta
+    scaled = gamma * Z_norm + beta
+
+    return scaled
