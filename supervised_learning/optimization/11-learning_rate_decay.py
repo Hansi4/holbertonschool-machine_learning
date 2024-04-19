31ce3f137762f49e8e8083100006feeec1f329b4
Hansi Laro <hansilaro48@gmail.com>
2024-04-19 18:26:25 +0000

    Task 11

--- supervised_learning/optimization/11-learning_rate_decay.py
+++ supervised_learning/optimization/11-learning_rate_decay.py
@@ -8,7 +8,8 @@ import numpy as np
 def learning_rate_decay(alpha, decay_rate, global_step, decay_step):
     """ A python function that updates the learning rate
     using inverse time decay in numpy """
-
-    
-
-    return 
+    # calculate factor that increases over time
+    factor = (1 + decay_rate * (global_step//decay_step))
+    # scale original learning rate by inverse of the factor
+    alpha = alpha / factor
+    return alpha
