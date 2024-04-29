a9c625c5fa2af063cc506704a8e12ec0cebf2a7d
Hansi Laro <hansilaro48@gmail.com>
2024-04-29 14:55:40 +0000

    Task 2

---
+++ supervised_learning/regularization/2-l2_reg_cost.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+""" L2 Regularization Cost """
+
+
+import numpy as np
+import tensorflow.compat.v1 as tf
+
+
+def l2_reg_cost(cost):
+    """ A python function that calculates the cost
+    of a neural network with L2 regularization """
+
+    reg_term = 0
+    for i in range(1, L+1):
+        W_i = weights['W' + str(i)]
+        reg_term += np.sum(np.square(W_i))
+
+    cost_L2 = cost + (lambtha / (2*m)) * reg_term
+
+    return cost_L2
