8c2491b5d5d2c445badfc878c491ff9d4de3cc9d
Hansi Laro <hansilaro48@gmail.com>
2024-05-06 16:36:35 +0000

    Task 3

---
+++ supervised_learning/cnn/3-pool_backward.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+""" Pooling Back Prop """
+
+
+import numpy as np
+
+
+def pool_backward(dA, A_prev, kernel_shape, stride=(1, 1), mode='max'):
+    """ A python function that performs back propagation
+    over a pooling layer of a neural network """
+
+    
+
+
+
+    return dA_prev
