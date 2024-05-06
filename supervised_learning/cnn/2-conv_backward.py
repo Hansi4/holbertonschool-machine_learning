7d3b611b8fd788307a3bd7e8d8921510fee6841a
Hansi Laro <hansilaro48@gmail.com>
2024-05-06 16:33:12 +0000

    Task 2

---
+++ supervised_learning/cnn/2-conv_backward.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+""" Convolutional Back Prop """
+
+
+import numpy as np
+
+
+def conv_backward(dZ, A_prev, W, b, padding="same", stride=(1, 1)):
+    """ A python function that performs back propagation
+    over a convolutional layer of a neural network """
+
+    
+
+
+
+    return dA, dW, db
