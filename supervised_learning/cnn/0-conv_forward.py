9ac54f0d82035f2dcd1230977b23ecf94a61fb99
Hansi Laro <hansilaro48@gmail.com>
2024-05-06 16:26:49 +0000

    Task 0

---
+++ supervised_learning/cnn/0-conv_forward.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+""" Convolutional Forward Prop """
+
+
+import numpy as np
+
+
+def conv_forward(A_prev, W, b, activation, padding="same", stride=(1, 1)):
+    """ A python function that performs forward propagation
+    over a convolutional layer of a neural network """
+
+    
+
+
+
+    # apply activation function
+    Z = activation(Z)
+
+    return Z
