5cac8179b42611088331da1ab091399c90ae4914
Hansi Laro <hansilaro48@gmail.com>
2024-05-06 16:30:34 +0000

    Task 1

---
+++ supervised_learning/cnn/1-pool_forward.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+""" Pooling Forward Prop """
+
+
+import numpy as np
+
+
+def pool_forward(A_prev, kernel_shape, stride=(1, 1), mode='max'):
+    """ A python function that performs forward
+    propagation over a pooling layer of a neural network """
+
+    
+
+
+
+    return pooled_img
