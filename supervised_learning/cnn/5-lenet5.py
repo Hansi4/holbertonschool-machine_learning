8a78b837586d8e9d87f3a28681b6cdbd26d518ab
Hansi Laro <hansilaro48@gmail.com>
2024-05-06 16:43:58 +0000

    Task 5

---
+++ supervised_learning/cnn/5-lenet5.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+""" LeNet-5 (Keras) """
+
+
+import numpy as np
+import tensorflow.keras as K
+
+
+def lenet5(X):
+    """ A python function that builds a modified
+    version of the LeNet-5 architecture using keras """
+
+    
+
+
+
+    return 
