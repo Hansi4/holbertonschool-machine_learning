9fdbf5de4d5db589d2e1c0676cfb0787904c6f4c
Hansi Laro <hansilaro48@gmail.com>
2024-05-06 16:41:38 +0000

    Task 4

---
+++ supervised_learning/cnn/4-lenet5.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+""" LeNet-5 (Tensorflow 1) """
+
+
+import numpy as np
+import tensorflow.compat.v1 as tf
+
+
+def lenet5(x, y):
+    """ A python function that builds a modified
+    version of the LeNet-5 architecture using tensorflow """
+
+    
+
+
+
+    return 
