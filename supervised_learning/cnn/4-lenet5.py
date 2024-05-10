5f9a9bd63529c6e5905deec136d5101e78d3aa74
Hansi Laro <hansilaro48@gmail.com>
2024-05-10 16:38:11 +0000

    Task 4

--- supervised_learning/cnn/4-lenet5.py
+++ supervised_learning/cnn/4-lenet5.py
@@ -10,6 +10,77 @@ def lenet5(x, y):
     """ A python function that builds a modified
     version of the LeNet-5 architecture using tensorflow """
 
+    # Convolutional layer 1
+    conv1 = tf.layers.conv2d(
+        inputs=x,
+        filters=6,
+        kernel_size=(5, 5),
+        padding='same',
+        activation=tf.nn.relu,
+        kernel_initializer=tf.keras.initializers.VarianceScaling(scale=2.0),
+        name='conv1'
+    )
+
+    # Max pooling layer 1
+    pool1 = tf.layers.max_pooling2d(
+        inputs=conv1,
+        pool_size=(2, 2),
+        strides=(2, 2),
+        name='pool1'
+    )
+
+    # Convolutional layer 2
+    conv2 = tf.layers.conv2d(
+        inputs=pool1,
+        filters=16,
+        kernel_size=(5, 5),
+        padding='valid',
+        activation=tf.nn.relu,
+        kernel_initializer=tf.keras.initializers.VarianceScaling(scale=2.0),
+        name='conv2'
+    )
+
+    # Max pooling layer 2
+    pool2 = tf.layers.max_pooling2d(
+        inputs=conv2,
+        pool_size=(2, 2),
+        strides=(2, 2),
+        name='pool2'
+    )
+
+    # Flatten layer
+    flatten = tf.layers.flatten(pool2)
+
+    # Fully connected layer 1
+    fc1 = tf.layers.dense(
+        inputs=flatten,
+        units=120,
+        activation=tf.nn.relu,
+        kernel_initializer=tf.keras.initializers.VarianceScaling(scale=2.0),
+        name='fc1'
+    )
+
+    # Fully connected layer 2
+    fc2 = tf.layers.dense(
+        inputs=fc1,
+        units=84,
+        activation=tf.nn.relu,
+        kernel_initializer=tf.keras.initializers.VarianceScaling(scale=2.0),
+        name='fc2'
+    )
+
+    # Output layer
+    logits = tf.layers.dense(
+        inputs=fc2,
+        units=10,
+        activation=None,
+        kernel_initializer=tf.keras.initializers.VarianceScaling(scale=2.0),
+        name='logits'
+    )
+
+    # Softmax activation
+    softmax_output = tf.nn.softmax(logits, name='softmax_output')
+
     
 
 
