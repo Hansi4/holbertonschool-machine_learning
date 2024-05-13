e3459f78b5e47980eeb8a7a7647e3a4cb40821d9
Hansi Laro <hansilaro48@gmail.com>
2024-05-13 14:17:20 +0000

    Task 5

--- supervised_learning/cnn/5-lenet5.py
+++ supervised_learning/cnn/5-lenet5.py
@@ -2,7 +2,6 @@
 """ LeNet-5 (Keras) """
 
 
-import numpy as np
 import tensorflow.keras as K
 
 
@@ -10,8 +9,37 @@ def lenet5(X):
     """ A python function that builds a modified
     version of the LeNet-5 architecture using keras """
 
-    
+    initializer = K.initializers.HeNormal()
 
+    model = K.Sequential([
+        K.layers.Conv2D(filters=6,
+                             kernel_size=5,
+                             padding='same',
+                             kernel_initializer=initializer,
+                             activation='relu'),
+        K.layers.MaxPooling2D(pool_size=2,
+                                   strides=2),
+        K.layers.Conv2D(filters=16,
+                             kernel_size=5,
+                             padding='valid',
+                             kernel_initializer=initializer,
+                             activation='relu'),
+        K.layers.MaxPooling2D(pool_size=2,
+                                   strides=2),
+        K.layers.Flatten(),
+        K.layers.Dense(120,
+                       activation='relu',
+                       kernel_initializer=initializer),
+        K.layers.Dense(84,
+                       activation='relu',
+                       kernel_initializer=initializer),
+        K.layers.Dense(10,
+                       activation='softmax',
+                       kernel_initializer=initializer)
+    ])
 
+    model.compile(loss='categorical_crossentropy',
+                  optimizer=K.optimizers.Adam(),
+                  metrics=['accuracy'])
 
-    return 
+    return model
