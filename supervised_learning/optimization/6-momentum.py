b6582ee737b6913b1c5d88bed4d5b0b097f451b5
Hansi Laro <hansilaro48@gmail.com>
2024-05-13 13:17:47 +0000

    Task 6

--- supervised_learning/optimization/6-momentum.py
+++ supervised_learning/optimization/6-momentum.py
@@ -1,27 +1,24 @@
 #!/usr/bin/env python3
 """
-   Momentum upgraded
+   Momentum
 """
+
 
 import tensorflow as tf
 
 
-def create_momentum_op(loss, alpha, beta1):
+def create_momentum_op(alpha, beta1):
     """
-        Method to creates the training operation for a NN
-        in tf using gradient descent with momentum opt algo
+    Sets up the gradient descent with momentum
 
-        :param loss: loss of network
-        :param alpha: learning rate
-        :param beta1: momentum weight
+    Args:
+        alpha (float): The learning rate.
+        beta1 (float): The momentum weight.
 
-        :return: momentum optimization operation
+    Returns:
+        optimizer: Optimizer object for gradient descent with momentum.
     """
-    # set optimizer taht implement Momentum algo in tf
-    optimizer = tf.train.MomentumOptimizer(learning_rate=alpha,
-                                           momentum=beta1)
-
-    # train_op to minimize loss with this optimizer
-    train_op = optimizer.minimize(loss)
-
-    return train_op
+    optimizer = tf.keras.optimizers.SGD(
+        learning_rate=alpha,
+        momentum=beta1)
+    return optimizer
