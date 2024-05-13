be1c67a0461a114760648590b3916fe0c79ccd63
Hansi Laro <hansilaro48@gmail.com>
2024-05-13 14:02:06 +0000

    Task 4

--- supervised_learning/cnn/4-lenet5.py
+++ supervised_learning/cnn/4-lenet5.py
@@ -2,7 +2,6 @@
 """ LeNet-5 (Tensorflow 1) """
 
 
-import numpy as np
 import tensorflow.compat.v1 as tf
 
 
@@ -11,78 +10,82 @@ def lenet5(x, y):
     version of the LeNet-5 architecture using tensorflow """
 
     # Convolutional layer 1
-    conv1 = tf.layers.conv2d(
+    conv1 = tf.layers.Conv2D(
         inputs=x,
         filters=6,
         kernel_size=(5, 5),
         padding='same',
         activation=tf.nn.relu,
-        kernel_initializer=tf.keras.initializers.VarianceScaling(scale=2.0),
-        name='conv1'
+        kernel_initializer=tf.keras.initializers.VarianceScaling(scale=2.0)
     )
 
     # Max pooling layer 1
-    pool1 = tf.layers.max_pooling2d(
+    pool1 = tf.layers.MaxPooling2D(
         inputs=conv1,
         pool_size=(2, 2),
-        strides=(2, 2),
-        name='pool1'
+        strides=(2, 2)
     )
 
     # Convolutional layer 2
-    conv2 = tf.layers.conv2d(
+    conv2 = tf.layers.Conv2D(
         inputs=pool1,
         filters=16,
         kernel_size=(5, 5),
         padding='valid',
         activation=tf.nn.relu,
-        kernel_initializer=tf.keras.initializers.VarianceScaling(scale=2.0),
-        name='conv2'
+        kernel_initializer=tf.keras.initializers.VarianceScaling(scale=2.0)
     )
 
     # Max pooling layer 2
-    pool2 = tf.layers.max_pooling2d(
+    pool2 = tf.layers.MaxPooling2D(
         inputs=conv2,
         pool_size=(2, 2),
-        strides=(2, 2),
-        name='pool2'
+        strides=(2, 2)
     )
 
     # Flatten layer
-    flatten = tf.layers.flatten(pool2)
+    flat = tf.layers.Flatten()(pool2)
 
     # Fully connected layer 1
-    fc1 = tf.layers.dense(
-        inputs=flatten,
+    fc1 = tf.layers.Dense(
+        inputs=flat,
         units=120,
         activation=tf.nn.relu,
-        kernel_initializer=tf.keras.initializers.VarianceScaling(scale=2.0),
-        name='fc1'
+        kernel_initializer=tf.keras.initializers.VarianceScaling(scale=2.0)
     )
 
     # Fully connected layer 2
-    fc2 = tf.layers.dense(
+    fc2 = tf.layers.Dense(
         inputs=fc1,
         units=84,
         activation=tf.nn.relu,
-        kernel_initializer=tf.keras.initializers.VarianceScaling(scale=2.0),
-        name='fc2'
+        kernel_initializer=tf.keras.initializers.VarianceScaling(scale=2.0)
     )
 
     # Output layer
-    logits = tf.layers.dense(
+    output = tf.layers.Dense(
         inputs=fc2,
         units=10,
         activation=None,
-        kernel_initializer=tf.keras.initializers.VarianceScaling(scale=2.0),
-        name='logits'
+        kernel_initializer=tf.keras.initializers.VarianceScaling(scale=2.0)
     )
 
     # Softmax activation
-    softmax_output = tf.nn.softmax(logits, name='softmax_output')
+    softmax_output = tf.nn.softmax(output)
 
-    
+    loss = tf.losses.softmax_cross_entropy(
+        onehot_labels = y,
+        logits = output
+    )
 
+    train_Adam = tf.train.AdamOptimizer().minimize(loss)
 
+    y_pred = tf.argmax(output, axis=1)
+    y_true = tf.argmax(y, axis=1)
+    correct_prediction = tf.equal(y_pred, y_true)
 
-    return 
+    correct_prediction = tf.cast(correct_prediction, dtype=tf.float32)
+
+    accuracy = tf.reduce_mean(correct_prediction)
+
+    return softmax, train_Adam, loss, accuracy
