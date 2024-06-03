304fd1b5ec93314b7080c8a545a6f575695f47aa
Hansi Laro <hansilaro48@gmail.com>
2024-06-03 14:45:32 +0000

    Task 0

--- supervised_learning/deep_cnns/0-inception_block.py
+++ supervised_learning/deep_cnns/0-inception_block.py
@@ -10,22 +10,44 @@ def inception_block(A_prev, filters):
     as described in Going Deeper with Convolutions (2014) """
     F1, F3R, F3, F5R, F5, FPP = filters
 
-    # 1x1 convolution
-    conv1x1 = tf.keras.layers.Conv2D(F1, (1, 1), padding='same', activation='relu')(A_prev)
-    
-    # 1x1 convolution followed by 3x3 convolution
-    conv3x3_reduce = tf.keras.layers.Conv2D(F3R, (1, 1), padding='same', activation='relu')(A_prev)
-    conv3x3 = tf.keras.layers.Conv2D(F3, (3, 3), padding='same', activation='relu')(conv3x3_reduce)
-    
-    # 1x1 convolution followed by 5x5 convolution
-    conv5x5_reduce = tf.keras.layers.Conv2D(F5R, (1, 1), padding='same', activation='relu')(A_prev)
-    conv5x5 = tf.keras.layers.Conv2D(F5, (5, 5), padding='same', activation='relu')(conv5x5_reduce)
-    
-    # 3x3 max pooling followed by 1x1 convolution
-    maxpool = tf.keras.layers.MaxPooling2D((3, 3), strides=(1, 1), padding='same')(A_prev)
-    maxpool_conv = tf.keras.layers.Conv2D(FPP, (1, 1), padding='same', activation='relu')(maxpool)
-    
-    # Concatenate all the filters
-    output = tf.keras.layers.concatenate([conv1x1, conv3x3, conv5x5, maxpool_conv], axis=-1)
-    
-    return output
+    layer_1 = K.layers.Conv2D(filters=F1,
+                              kernel_size=(1, 1),
+                              padding='same',
+                              activation=K.activations.relu,
+                              kernel_initializer=K.initializers.he_normal())
+    output_1 = layer_1(A_prev)
+    layer_2 = K.layers.Conv2D(filters=F3R,
+                              kernel_size=(1, 1),
+                              padding='same',
+                              activation=K.activations.relu,
+                              kernel_initializer=K.initializers.he_normal())
+    output_2 = layer_2(A_prev)
+    layer_3 = K.layers.Conv2D(filters=F3,
+                              kernel_size=(3, 3),
+                              padding='same',
+                              activation=K.activations.relu,
+                              kernel_initializer=K.initializers.he_normal())
+    output_3 = layer_3(output_2)
+    layer_4 = K.layers.Conv2D(filters=F5R,
+                              kernel_size=(1, 1),
+                              padding='same',
+                              activation=K.activations.relu,
+                              kernel_initializer=K.initializers.he_normal())
+    output_4 = layer_4(A_prev)
+    layer_5 = K.layers.Conv2D(filters=F5,
+                              kernel_size=(5, 5),
+                              padding='same',
+                              activation=K.activations.relu,
+                              kernel_initializer=K.initializers.he_normal())
+    output_5 = layer_5(output_4)
+    layer_6 = K.layers.MaxPooling2D(pool_size=(3, 3),
+                                    strides=(1, 1),
+                                    padding='same')
+    output_6 = layer_6(A_prev)
+    layer_7 = K.layers.Conv2D(filters=FPP,
+                              kernel_size=(1, 1),
+                              padding='same',
+                              activation=K.activations.relu,
+                              kernel_initializer=K.initializers.he_normal())
+    output_7 = layer_7(output_6)
+    return (K.layers.concatenate([output_1, output_3, output_5, output_7]))
