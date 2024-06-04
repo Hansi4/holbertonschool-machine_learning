d3ce6900c32f463f3b242127f86fc95caf7aa5c7
Hansi Laro <hansilaro48@gmail.com>
2024-06-04 17:12:53 +0000

    Task 0

--- supervised_learning/deep_cnns/0-inception_block.py
+++ supervised_learning/deep_cnns/0-inception_block.py
@@ -1,18 +1,14 @@
 #!/usr/bin/env python3
-"""
-Defines a function that builds an inception block
-using Keras
-"""
+""" Inception Block """
 
 
 import tensorflow.keras as K
 
 
 def inception_block(A_prev, filters):
-    """
-    Builds an inception block using Keras
+    """ A python function that builds an inception block as
+    described in Going Deeper with Convolutions (2014)"""
 
-    """
     F1, F3R, F3, F5R, F5, FPP = filters
     layer_1 = K.layers.Conv2D(filters=F1,
                               kernel_size=(1, 1),
