7159e18b20a202cd9c364326ac866d45f0f6265c
Hansi Laro <hansilaro48@gmail.com>
2024-06-09 22:40:57 +0000

    Task 6

---
+++ supervised_learning/deep_cnns/6-transition_layer.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+""" Transition Layer """
+
+
+from tensorflow import keras as K
+
+
+def transition_layer(X, nb_filters, compression):
+    """ A python function that builds a transition layer as
+    described in Densely Connected Convolutional Networks """
+
+    init = K.initializers.he_normal(seed=0)
+
+    
+   
+
+    return model
