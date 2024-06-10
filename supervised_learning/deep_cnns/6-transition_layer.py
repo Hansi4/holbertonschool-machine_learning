355cffa5474f0f3ec33986ec19cb6834dab1afd0
Hansi Laro <hansilaro48@gmail.com>
2024-06-10 20:36:09 +0000

    Task 6

--- supervised_learning/deep_cnns/6-transition_layer.py
+++ supervised_learning/deep_cnns/6-transition_layer.py
@@ -10,8 +10,19 @@ def transition_layer(X, nb_filters, compression):
     described in Densely Connected Convolutional Networks """
 
     init = K.initializers.he_normal(seed=0)
+    nb_filters = int(nb_filters * compression)
 
-    
-   
+    # 1x1 conv
+    X = K.layers.BatchNormalization(axis=3)(X)
+    X = K.layers.Activation('relu')(X)
+    X = K.layers.Conv2D(nb_filters,
+                        (1, 1),
+                        padding='same',
+                        kernel_initializer=init)(X)
 
-    return model
+    # average pool
+    X = K.layers.AveragePooling2D((2, 2),
+                                  strides=(2, 2),
+                                  padding='same')(X)
+
+    return X, nb_filters
