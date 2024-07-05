1282c6664314721e9c61dd032815a1d6cce995eb
Hansi Laro <hansilaro48@gmail.com>
2024-07-05 11:03:07 +0000

    Task 1

--- supervised_learning/object_detection/1-yolo.py
+++ supervised_learning/object_detection/1-yolo.py
@@ -8,10 +8,11 @@ class Yolo:
     """ A class Yolo that uses the Yolo v3 algorithm to perform object detection """
     def __init__(self, model_path, classes_path, class_t, nms_t, anchors):
         self.model = K.models.load_model(model_path)
-        with open(classes_path, 'r') as classes_file:
-            self.class_names = [
-                line.strip() for line in classes_file.readlines()
-                ]
+        self.class_names = []
+        with open(classes_path, 'r') as f:
+            for line in f:
+                line = line.strip()
+                self.class_names.append(line)
         self.class_t = class_t
         self.nms_t = nms_t
         self.anchors = anchors
