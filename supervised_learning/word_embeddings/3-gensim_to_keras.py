7dbf4ccf5f77664943895ef9d51aa268090a8dd4
Hansi Laro <hansilaro48@gmail.com>
2024-08-05 17:29:31 +0000

    Task 3

---
+++ supervised_learning/word_embeddings/3-gensim_to_keras.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+""" Extract Word2Vec Module """
+
+
+def gensim_to_keras(model):
+    """ A python function that converts a gensim
+    word2vec model to a keras Embedding layer """
+
+    return model.wv.get_keras_embedding(True)
