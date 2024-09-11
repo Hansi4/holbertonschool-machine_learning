70a67cfabdd1120a848dccf67a8adce36778828a
Hansi Laro <hansilaro48@gmail.com>
2024-09-11 16:45:44 +0000

    Task 0

--- supervised_learning/word_embeddings/0-bag_of_words.py
+++ supervised_learning/word_embeddings/0-bag_of_words.py
@@ -1,11 +1,21 @@
 #!/usr/bin/env python3
-""" Bag Of Words Module """
+"""Bag of Words Module"""
 import numpy as np
 import re
 
 
 def bag_of_words(sentences, vocab=None):
-    """ A python function that creates a bag of words embedding matrix """
+    """Creates a bag of words embedding matrix:
+
+    sentences is a list of sentences to analyze
+    vocab is a list of the vocabulary words to use for the analysis
+    If None, all words within sentences should be used
+
+    Returns: embeddings, features
+    embeddings is a numpy.ndarray of shape (s, f) containing the embeddings
+    s is the number of sentences in sentences
+    f is the number of features analyzed
+    features is a list of the features used for embeddings"""
 
     if vocab is None:
         vocab = []
