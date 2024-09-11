ef0088175c549b52bff918822ad2630e9a3e9d47
Hansi Laro <hansilaro48@gmail.com>
2024-09-11 16:41:59 +0000

    Task 0

--- supervised_learning/nlp_metrics/0-uni_bleu.py
+++ supervised_learning/nlp_metrics/0-uni_bleu.py
@@ -1,21 +1,18 @@
 #!/usr/bin/env python3
-"""Unigram BLEU Score"""
+""" Unigram BLEU score """
 import numpy as np
 
 
 def uni_bleu(references, sentence):
-    """
-    Calculates the unigram BLEU score for a sentence
-        - references is a list of reference translations
-        - each reference translation is a list of the words in the translation
-        - sentence is a list containing the model proposed sentence
+    """ A python function that calculates
+    the unigram BLEU score for a sentence """
 
-    Returns: the unigram BLEU score
-    """
+    Brevity_Penalty = min(1, np.exp(1 -
+                          len(min(references, key=len)) / len(sentence)))
 
-    BP = min(1, np.exp(1 - len(min(references, key=len)) / len(sentence)))
-
-    precision = max([sum(match in reference for match in set(sentence))
+    Precision = max([sum(match in reference for match in set(sentence))
                      for reference in references]) / len(sentence)
 
-    return BP * np.exp(np.log(precision))
+    BLEU_Score = Brevity_Penalty * np.exp(np.log(Precision))
+
+    return BLEU_Score
