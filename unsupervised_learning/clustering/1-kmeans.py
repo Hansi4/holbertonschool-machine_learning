3f3d6308443888f8143e7c071e914dcd5f5c2e9b
Hansi Laro <hansilaro48@gmail.com>
2024-09-17 15:31:51 +0000

    Task 1

--- unsupervised_learning/clustering/1-kmeans.py
+++ unsupervised_learning/clustering/1-kmeans.py
@@ -37,7 +37,7 @@ def kmeans(X, k, iterations=1000):
             if X[clss == c].size == 0:
                 C[c] = np.random.uniform(X_min, X_max, size=(1, d))
             else:
-            C[c] = X[clss == c].mean(axis=0)
+                C[c] = X[clss == c].mean(axis=0)
 
 
         centroids_extended = C[:, np.newaxis]
