74d318ed81e5572c0cd00d2924b9055840322c97
Hansi Laro <hansilaro48@gmail.com>
2024-09-23 20:01:23 +0000

    Task 3

--- supervised_learning/word_embeddings/3-gensim_to_keras.py
+++ supervised_learning/word_embeddings/3-gensim_to_keras.py
@@ -2,11 +2,14 @@
 """ Extract Word2Vec Module """
 import tensorflow as tf
 
+
 def gensim_to_keras(model):
     # Get the vocabulary and the weights from the Gensim model
     vocab_size = len(model.wv)
     embedding_dim = model.wv.vector_size
-    weights = tf.zeros((vocab_size, embedding_dim))
+    
+    # Create a TensorFlow variable to hold the weights
+    weights = tf.Variable(tf.zeros((vocab_size, embedding_dim)), trainable=False)
     
     # Populate the weights matrix using TensorFlow
     for i, word in enumerate(model.wv.index_to_key):
@@ -15,7 +18,7 @@ def gensim_to_keras(model):
     # Create a Keras Embedding layer with trainable weights
     embedding_layer = tf.keras.layers.Embedding(input_dim=vocab_size,
                                                 output_dim=embedding_dim,
-                                                embeddings_initializer=tf.keras.initializers.Constant(weights),
+                                                embeddings_initializer=tf.keras.initializers.Constant(weights.numpy()),
                                                 trainable=True)
     
     return embedding_layer
