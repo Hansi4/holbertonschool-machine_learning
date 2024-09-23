042e04f91e24cc9cfd75d9272169b57f9b790df5
Hansi Laro <hansilaro48@gmail.com>
2024-09-23 10:58:09 +0000

    Task 1

--- unsupervised_learning/clustering/1-kmeans.py
+++ unsupervised_learning/clustering/1-kmeans.py
@@ -14,58 +14,31 @@ def kmeans(X, k, iterations=1000):
     if not isinstance(iterations, int) or iterations <= 0:
         return None, None
 
-    # Setting min and max values per col
     n, d = X.shape
     X_min = X.min(axis=0)
     X_max = X.max(axis=0)
 
-    # Centroid
     C = np.random.uniform(X_min, X_max, size=(k, d))
 
-    # Loop for the maximum number of iterations
     for i in range(iterations):
 
-        # 1. Initialize
         centroids = np.copy(C)
         centroids_extended = C[:, np.newaxis]
 
-        # 2. Calculate distance
-        distances = np.sqrt((((X - centroids_extended) ** 2).sum(axis=2)))
+        distances = np.sqrt(((X - centroids_extended) ** 2).sum(axis=2))
         clss = np.argmin(distances, axis=0)
 
-        # Reassign points to closest centroid
         for c in range(k):
-            if X[clss == c].size == 0:
+            if X[clss == c].size == 0:  # cluster is empty
                 C[c] = np.random.uniform(X_min, X_max, size=(1, d))
             else:
                 C[c] = X[clss == c].mean(axis=0)
 
         centroids_extended = C[:, np.newaxis]
-        distances = np.sqrt((((X - centroids_extended) ** 2).sum(axis=2)))
+        distances = np.sqrt(((X - centroids_extended) ** 2).sum(axis=2))
         clss = np.argmin(distances, axis=0)
-
 
         if (centroids == C).all():
             break
 
     return C, clss
-
-
-
-
-
-
-
-
-
-
-
-def variance(X, C):
-    n, d = X.shape
-    centroids_extended = C[:, np.newaxis]
-    distances = np.sqrt((((X - centroids_extended) ** 2).sum(axis=2)))
-
-    min_distanes = np.min(distances, axis=0)
-    variances = np.sum(min_distanes ** 2)
-
-    return variance
