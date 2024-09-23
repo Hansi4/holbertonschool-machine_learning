a43e9f13b7b4ec46e50e6fd61a4ad321df49ea1d
Hansi Laro <hansilaro48@gmail.com>
2024-09-23 18:18:06 +0000

    Task 8

--- unsupervised_learning/clustering/8-EM.py
+++ unsupervised_learning/clustering/8-EM.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python3
 """ Final step iterate E and M to get EM algorithm """
+import numpy as np
+initialize = __import__('4-initialize').initialize
+expectation = __import__('6-expectation').expectation
+maximization = __import__('7-maximization').maximization
 
 
 def expectation_maximization(X, k, iterations=1000, tol=1e-5, verbose=False):
