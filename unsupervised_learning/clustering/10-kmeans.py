346541983232149ea446fafb4c0eaccc97e31023
Hansi Laro <hansilaro48@gmail.com>
2024-09-23 14:29:57 +0000

    Task 10

--- unsupervised_learning/clustering/10-kmeans.py
+++ unsupervised_learning/clustering/10-kmeans.py
@@ -7,5 +7,5 @@ def kmeans(X, k):
     """ A python function that performs K-means on a dataset """
     kmeans = sklearn.cluster.KMeans(n_clusters=k)
     kmeans.fit(X)
-    
+
     return kmeans.cluster_centers_, kmeans.labels_
