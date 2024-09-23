64ae91a18fb42ab252fabe31eeda24c8b3b29d64
Hansi Laro <hansilaro48@gmail.com>
2024-09-23 20:06:15 +0000

    Task 3

--- supervised_learning/word_embeddings/3-gensim_to_keras.py
+++ supervised_learning/word_embeddings/3-gensim_to_keras.py
@@ -4,17 +4,19 @@ import tensorflow as tf
 
 
 def gensim_to_keras(model):
-    # Get the vocabulary and the weights from the Gensim model
+    # Get the vocabulary size and embedding dimension
     vocab_size = len(model.wv)
     embedding_dim = model.wv.vector_size
     
-    # Create a NumPy array to hold the weights
+    # Get the weights from the Gensim model as a TensorFlow tensor
     weights = tf.convert_to_tensor(model.wv.vectors, dtype=tf.float32)
 
     # Create a Keras Embedding layer with the weights
-    embedding_layer = tf.keras.layers.Embedding(input_dim=vocab_size,
-                                                output_dim=embedding_dim,
-                                                embeddings_initializer=tf.keras.initializers.Constant(weights),
-                                                trainable=True)
+    embedding_layer = tf.keras.layers.Embedding(
+        input_dim=vocab_size,
+        output_dim=embedding_dim,
+        embeddings_initializer=tf.keras.initializers.Constant(weights),
+        trainable=True  # Allow further training of the weights
+    )
     
     return embedding_layer
