7513152a8611024f17567b2e81ebf45d842a8fb5
Hansi Laro <hansilaro48@gmail.com>
2024-09-23 19:32:57 +0000

    Task 4

--- supervised_learning/word_embeddings/4-fasttext.py
+++ supervised_learning/word_embeddings/4-fasttext.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python3
 """ FastText Module """
-import genism
+import gensim
 
-
-def fasttext_model(sentences, vector_size=100, min_count=5, negative=5, window=5,
-                   cbow=True, epochs=5, seed=0, workers=1):
-    """ A python function that creates and trains a genism fastText model """
-
-    return genism.models.FastText(sentences=sentences, vector_size=vector_size, min_count=min_count, 
-                    window=window, negative=negative, epochs=epochs,
-                    seed=seed, workers=workers, sg=not cbow)
+def fasttext_model(sentences, size=100, min_count=5, negative=5, window=5,
+                   cbow=True, iterations=5, seed=0, workers=1):
+    """ A python function that creates and trains a gensim FastText model """
+    return gensim.models.FastText(sentences=sentences, vector_size=size, 
+                                   min_count=min_count, window=window, 
+                                   negative=negative, epochs=iterations,
+                                   seed=seed, workers=workers, sg=not cbow)
