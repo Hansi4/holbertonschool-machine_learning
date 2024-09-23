149731a642e5836f6668a8cbf1400da883fc3af9
Hansi Laro <hansilaro48@gmail.com>
2024-09-23 19:49:02 +0000

    Task 0

--- supervised_learning/word_embeddings/0-bag_of_words.py
+++ supervised_learning/word_embeddings/0-bag_of_words.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3
+""" Bag Of Words """
 import numpy as np
 from sklearn.feature_extraction.text import CountVectorizer
 
 
 def bag_of_words(sentences, vocab=None):
+    """ A python function that creates a bag of words embedding matrix """
+
     if vocab is None:
         vectorizer = CountVectorizer()
     else:
