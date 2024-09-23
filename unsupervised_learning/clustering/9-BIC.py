f95417c9df445012130a7fec125e09358d59f9e8
Hansi Laro <hansilaro48@gmail.com>
2024-09-23 18:35:18 +0000

    Task 9

--- unsupervised_learning/clustering/9-BIC.py
+++ unsupervised_learning/clustering/9-BIC.py
@@ -8,48 +8,38 @@ def BIC(X, kmin=1, kmax=None, iterations=1000, tol=1e-5, verbose=False):
     """ A python function that finds the best number of
     clusters for a GMM using the Bayesian Information Criterion """
 
-    if not isinstance(X, np.ndarray) or len(X.shape) != 2:
-        return None, None, None, None
-    if type(kmin) != int or kmin <= 0 or kmin >= X.shape[0]:
-        return None, None, None, None
-    if type(kmax) != int or kmax <= 0 or kmax >= X.shape[0]:
-        return None, None, None, None
-    if kmin >= kmax:
-        return None, None, None, None
-    if type(iterations) != int or iterations <= 0:
-        return None, None, None, None
-    if type(tol) != float or tol <= 0:
-        return None, None, None, None
-    if type(verbose) != bool:
-        return None, None, None, None
+    try:
+        if kmax == 1:
+            return None, None, None, None
+        n, d = X.shape
+        if kmax is None:
+            kmax = n
+            if kmax >= kmin:
+                return None, None, None, None
 
-    k_best = []
-    best_res = []
-    logl_val = []
-    bic_val = []
-    n, d = X.shape
-    for k in range(kmin, kmax + 1):
-        pi, m, S,  _, log_l = expectation_maximization(X, k, iterations, tol,
+        k_history = list(range(kmin, kmax+1))
+        results_history = []
+        lh_history = []
+        bic_history = []
+
+        for k in range(kmin, kmax+1):
+            pi, m, S, g, lh = expectation_maximization(X, k, iterations, tol,
                                                        verbose)
-        k_best.append(k)
-        best_res.append((pi, m, S))
-        logl_val.append(log_l)
 
-        # Formula pf paramaters: https://bit.ly/33Cw8lH
-        # code based on gaussian mixture source code n_parameters source code
-        cov_params = k * d * (d + 1) / 2.
-        mean_params = k * d
-        p = int(cov_params + mean_params + k - 1)
+            if pi is None or m is None or S is None or g is None or lh is None:
+                return None, None, None, None
 
-        # Formula for this task BIC = p * ln(n) - 2 * l
-        bic = p * np.log(n) - 2 * log_l
-        bic_val.append(bic)
+            num_parameters = k + k * d + k * d * (d + 1) // 2 - 1
+            bic = num_parameters * np.log(n) - 2 * lh
 
-    bic_val = np.array(bic_val)
-    logl_val = np.array(logl_val)
-    best_val = np.argmin(bic_val)
+            lh_history.append(lh)
+            results_history.append((pi, m, S))
+            bic_history.append(bic)
 
-    k_best = k_best[best_val]
-    best_res = best_res[best_val]
+        min_bic_index = np.argmin(bic_history)
+        best_k = k_history[min_bic_index]
+        best_result = results_history[min_bic_index]
 
-    return k_best, best_res, logl_val, bic_val
+        return best_k, best_result, np.array(lh_history), np.array(bic_history)
+    except Exception:
+        return None, None, None, None
