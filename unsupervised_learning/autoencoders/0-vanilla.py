aa7252303b33182c8c20faf75523ee8b7d50fea5
Hansi Laro <hansilaro48@gmail.com>
2024-09-24 17:22:54 +0000

    Task 0

--- unsupervised_learning/autoencoders/0-vanilla.py
+++ unsupervised_learning/autoencoders/0-vanilla.py
@@ -1,47 +1,46 @@
 #!/usr/bin/env python3
 """ "Vanilla" Autoencoder """
-import tensorflow as tf
-from tensorflow.keras import layers, models
+import tensorflow.keras as keras
 
 
 def autoencoder(input_dims, hidden_layers, latent_dims):
     """ A python function that creates an autoencoder """
 
     # Encoder
-    input_layer = layers.Input(shape=(input_dims,))
+    input_layer = keras.layers.Input(shape=(input_dims,))
     encoded = input_layer
     
     # Adding hidden layers to the encoder with ReLU activation
     for nodes in hidden_layers:
-        encoded = layers.Dense(nodes, activation='relu')(encoded)
+        encoded = keras.layers.Dense(nodes, activation='relu')(encoded)
     
     # Latent space representation
-    latent = layers.Dense(latent_dims, activation='relu')(encoded)
+    latent = keras.layers.Dense(latent_dims, activation='relu')(encoded)
     
     # Decoder
     decoded = latent
     
     # Adding hidden layers to the decoder (reverse of encoder)
     for nodes in reversed(hidden_layers):
-        decoded = layers.Dense(nodes, activation='relu')(decoded)
+        decoded = keras.layers.Dense(nodes, activation='relu')(decoded)
     
     # Output layer with Sigmoid activation for binary cross-entropy
-    output_layer = layers.Dense(input_dims, activation='sigmoid')(decoded)
+    output_layer = keras.layers.Dense(input_dims, activation='sigmoid')(decoded)
     
     # Models
-    encoder = models.Model(input_layer, latent, name="encoder")
-    decoder_input = layers.Input(shape=(latent_dims,))
+    encoder = keras.models.Model(input_layer, latent, name="encoder")
+    decoder_input = keras.layers.Input(shape=(latent_dims,))
     decoder_output = decoder_input
     
     # Build decoder layers
     for nodes in reversed(hidden_layers):
-        decoder_output = layers.Dense(nodes, activation='relu')(decoder_output)
-    decoder_output = layers.Dense(input_dims, activation='sigmoid')(decoder_output)
+        decoder_output = keras.layers.Dense(nodes, activation='relu')(decoder_output)
+    decoder_output = keras.layers.Dense(input_dims, activation='sigmoid')(decoder_output)
     
-    decoder = models.Model(decoder_input, decoder_output, name="decoder")
+    decoder = keras.models.Model(decoder_input, decoder_output, name="decoder")
     
     # Full autoencoder model
-    auto = models.Model(input_layer, decoder(encoder(input_layer)), name="autoencoder")
+    auto = keras.models.Model(input_layer, decoder(encoder(input_layer)), name="autoencoder")
     
     # Compile the autoencoder
     auto.compile(optimizer='adam', loss='binary_crossentropy')
