42464b615cf86947f31e093cc226afad6d98fa50
Hansi Laro <hansilaro48@gmail.com>
2024-09-24 17:13:30 +0000

    Task 2

---
+++ unsupervised_learning/autoencoders/2-convolutional.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+""" Convolutional Autoencoder """
+import numpy as np
+
+
+def autoencoder(input_dims, filters, latent_dims):
+    """ A python function that creates a convolutional autoencoder """
+
+    return encoder, decoder, auto
