ff8afc0d0383f1269218e3a98783c9a53435eff3
Hansi Laro <hansilaro48@gmail.com>
2024-09-24 17:10:20 +0000

    Task 0

---
+++ unsupervised_learning/autoencoders/0-vanilla.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+""" "Vanilla" Autoencoder """
+import numpy as np
+
+
+def autoencoder(input_dims, hidden_layers, latent_dims):
+    """ A python function that creates an autoencoder """
+
+    return encoder, decoder, auto
