8ac1969ac0e9f642255203d4a7be5142985c51ca
Hansi Laro <hansilaro48@gmail.com>
2024-09-24 17:11:53 +0000

    Task 1

---
+++ unsupervised_learning/autoencoders/1-sparse.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+""" Sparse Autoencoder """
+import numpy as np
+
+
+def autoencoder(input_dims, hidden_layers, latent_dims, lambtha):
+    """ A python function that creates a sparse autoencoder """
+
+    return encoder, decoder, auto
