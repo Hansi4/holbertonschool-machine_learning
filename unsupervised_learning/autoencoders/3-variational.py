30467dae2f58a399d1aedfece226823f641cbd1b
Hansi Laro <hansilaro48@gmail.com>
2024-09-24 17:15:12 +0000

    Task 3

---
+++ unsupervised_learning/autoencoders/3-variational.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+""" Variational Autoencoder """
+import numpy as np
+
+
+def autoencoder(input_dims, hidden_layers, latent_dims):
+    """ A python function that creates a variational autoencoder """
+
+    return encoder, decoder, auto
